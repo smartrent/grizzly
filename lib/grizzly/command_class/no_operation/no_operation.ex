b118a6ec51a34ac11b2a5e9f981d349a5d47d1fa
jf@collaboration-planners.com <jean.f.cloutier@gmail.com>
2019-08-26 10:28:32 -0400

    Support NoOperation command class Fixes SRHUB-122

---
+++ lib/grizzly/command_class/no_operation/no_operation.ex
@@ -0,0 +1,76 @@
+defmodule Grizzly.CommandClass.NoOperation do
+  @moduledoc """
+  Command module to work with the NO_OPERATION command class
+
+  Command Options:
+
+    * `:seq_number` - The sequence number of the Z/IP Packet
+    * `:retries` - The number of times to try to send the command (default 2)
+  """
+  @behaviour Grizzly.Command
+
+  alias Grizzly.Packet
+
+  @type t :: %__MODULE__{
+          seq_number: Grizzly.seq_number(),
+          retries: non_neg_integer()
+        }
+
+  @type opt ::
+          {:seq_number, Grizzly.seq_number()}
+          | {:retries, non_neg_integer()}
+
+  defstruct seq_number: nil,
+            retries: 2
+
+  @spec init([opt]) :: {:ok, t}
+  def init(opts) do
+    {:ok, struct(__MODULE__, opts)}
+  end
+
+  @spec encode(t) :: {:ok, binary}
+  def encode(%__MODULE__{seq_number: seq_number}) do
+    binary = Packet.header(seq_number) <> <<0x00>>
+
+    {:ok, binary}
+  end
+
+  @spec handle_response(t, Packet.t()) ::
+          {:continue, t} | {:done, {:error, :nack_response}} | {:done, :ok} | {:retry, t}
+  def handle_response(%__MODULE__{seq_number: seq_number}, %Packet{
+        seq_number: seq_number,
+        types: [:ack_response]
+      }) do
+    {:done, :ok}
+  end
+
+  def handle_response(%__MODULE__{seq_number: seq_number, retries: 0}, %Packet{
+        seq_number: seq_number,
+        types: [:nack_response]
+      }) do
+    {:done, {:error, :nack_response}}
+  end
+
+  def handle_response(%__MODULE__{seq_number: seq_number, retries: n} = command, %Packet{
+        seq_number: seq_number,
+        types: [:nack_response]
+      }) do
+    {:retry, %{command | retries: n - 1}}
+  end
+
+  def handle_response(
+        %__MODULE__{seq_number: seq_number} = command,
+        %Packet{
+          seq_number: seq_number,
+          types: [:nack_response, :nack_waiting]
+        } = packet
+      ) do
+    if Packet.sleeping_delay?(packet) do
+      {:queued, command}
+    else
+      {:continue, command}
+    end
+  end
+
+  def handle_response(command, _), do: {:continue, command}
+end
