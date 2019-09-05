5bda8288a5758601fcb7514b988cb52855b7e44a
Matt Ludwigs <mattludwigs@gmail.com>
2019-09-05 15:08:32 -0700

    Add command encode error

--- test/grizzly/command_class/switch_binary_test.exs
+++ test/grizzly/command_class/switch_binary_test.exs
@@ -5,11 +5,27 @@ defmodule Grizzly.CommandClass.SwitchBinary.Test do
 
   describe "encoding switch state" do
     test "on" do
-      assert 0xFF == SwitchBinary.encode_switch_state(:on)
+      assert {:ok, 0xFF} == SwitchBinary.encode_switch_state(:on)
     end
 
     test "off" do
-      assert 0x00 == SwitchBinary.encode_switch_state(:off)
+      assert {:ok, 0x00} == SwitchBinary.encode_switch_state(:off)
+    end
+
+    test "nil" do
+      assert {:error, :invalid_arg, nil} == SwitchBinary.encode_switch_state(nil)
+    end
+
+    test "number 1" do
+      assert {:error, :invalid_arg, 1} == SwitchBinary.encode_switch_state(1)
+    end
+
+    test "string grizzly" do
+      assert {:error, :invalid_arg, "grizzly"} == SwitchBinary.encode_switch_state("grizzly")
+    end
+
+    test "atom grizzly" do
+      assert {:error, :invalid_arg, :grizzly} == SwitchBinary.encode_switch_state(:grizzly)
     end
   end
 end
