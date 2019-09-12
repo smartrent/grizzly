0844d87b63af7acd999cb02899a9dc253c85e7f6
jf@collaboration-planners.com <jean.f.cloutier@gmail.com>
2019-09-12 10:25:12 -0400

    Encode errors for cc Basic Partially fixes SRHUB-100

--- lib/grizzly/command_class/basic.ex
+++ lib/grizzly/command_class/basic.ex
@@ -2,7 +2,8 @@ defmodule Grizzly.CommandClass.Basic do
   @type value :: :on | :off
   @type value_byte :: 0x00 | 0xFF
 
-  @spec encode_value(value) :: value_byte
-  def encode_value(:on), do: 0xFF
-  def encode_value(:off), do: 0x00
+  @spec encode_value(value) :: {:ok, value_byte} | {:error, :invalid_arg, any()}
+  def encode_value(:on), do: {:ok, 0xFF}
+  def encode_value(:off), do: {:ok, 0x00}
+  def encode_value(arg), do: {:error, :invalid_arg, arg}
 end
