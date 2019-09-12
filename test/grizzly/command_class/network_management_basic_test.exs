a725d30de94fe4bd06fa090bd71065ffc2e1371a
jf@collaboration-planners.com <jean.f.cloutier@gmail.com>
2019-09-12 13:37:41 -0400

    Encode errors for cc NetworkManagementBasic Partially fixes SRHUB-100

--- test/grizzly/command_class/network_management_basic_test.exs
+++ test/grizzly/command_class/network_management_basic_test.exs
@@ -3,6 +3,17 @@ defmodule Grizzly.CommandClass.NetworkManagementBasic.Test do
 
   alias Grizzly.CommandClass.NetworkManagementBasic
 
+  describe "encoding" do
+    test "encoding learn modes" do
+      assert {:ok, 0x00} = NetworkManagementBasic.encode_learn_mode(:disable)
+      assert {:ok, 0x01} = NetworkManagementBasic.encode_learn_mode(:enable)
+      assert {:ok, 0x02} = NetworkManagementBasic.encode_learn_mode(:enable_routed)
+
+      assert {:error, :invalid_arg, :fizzbuzz} ==
+               NetworkManagementBasic.encode_learn_mode(:fizzbuzz)
+    end
+  end
+
   describe "decoding default statuses" do
     test "decoding done status" do
       assert :done == NetworkManagementBasic.decode_default_set_status(0x06)
