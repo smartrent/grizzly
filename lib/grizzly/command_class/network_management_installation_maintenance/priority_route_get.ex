1c0bb6cca7aa0a428187f06b79b30966cab0f07d
jf@collaboration-planners.com <jean.f.cloutier@gmail.com>
2019-09-23 09:46:39 -0400

    Encodes errors for cc NetworkManagementInstallationMaintenance. Partially fixes SRHUB-100

--- lib/grizzly/command_class/network_management_installation_maintenance/priority_route_get.ex
+++ lib/grizzly/command_class/network_management_installation_maintenance/priority_route_get.ex
@@ -2,6 +2,7 @@ defmodule Grizzly.CommandClass.NetworkManagementInstallationMaintenance.Priority
   @behaviour Grizzly.Command
 
   alias Grizzly.Packet
+  alias Grizzly.Command.{EncodeError, Encoding}
   alias Grizzly.CommandClass.NetworkManagementInstallationMaintenance
 
   @type t :: %__MODULE__{
@@ -23,10 +24,15 @@ defmodule Grizzly.CommandClass.NetworkManagementInstallationMaintenance.Priority
     {:ok, struct(__MODULE__, opts)}
   end
 
-  @spec encode(t) :: {:ok, binary}
-  def encode(%__MODULE__{seq_number: seq_number, node_id: node_id}) do
-    binary = Packet.header(seq_number) <> <<0x67, 0x02, node_id>>
-    {:ok, binary}
+  @spec encode(t) :: {:ok, binary} | {:error, EncodeError.t()}
+  def encode(%__MODULE__{seq_number: seq_number, node_id: node_id} = command) do
+    with {:ok, _encoded} <-
+           Encoding.encode_and_validate_args(command, %{
+             node_id: :byte
+           }) do
+      binary = Packet.header(seq_number) <> <<0x67, 0x02, node_id>>
+      {:ok, binary}
+    end
   end
 
   @spec handle_response(t, Packet.t()) ::
