fc289758040caf8a2c4fc8aa18c7be54a37f1d6c
jf@collaboration-planners.com <jean.f.cloutier@gmail.com>
2019-09-23 19:45:14 -0400

    Encodes errors for cc ThermostatFanMode. Partially fixes SRHUB-100

--- lib/grizzly/command_class/thermostat_fan_mode.ex
+++ lib/grizzly/command_class/thermostat_fan_mode.ex
@@ -13,18 +13,20 @@ defmodule Grizzly.CommandClass.ThermostatFanMode do
           | :quiet
   @type fan_mode_byte :: 0x00..0x0A
 
-  @spec encode_thermostat_fan_mode(thermostat_fan_mode) :: fan_mode_byte
-  def encode_thermostat_fan_mode(:auto_low), do: 0x00
-  def encode_thermostat_fan_mode(:low), do: 0x01
-  def encode_thermostat_fan_mode(:auto_high), do: 0x02
-  def encode_thermostat_fan_mode(:high), do: 0x03
-  def encode_thermostat_fan_mode(:auto_medium), do: 0x04
-  def encode_thermostat_fan_mode(:medium), do: 0x05
-  def encode_thermostat_fan_mode(:circulation), do: 0x06
-  def encode_thermostat_fan_mode(:humidity_circulation), do: 0x07
-  def encode_thermostat_fan_mode(:left_right), do: 0x08
-  def encode_thermostat_fan_mode(:up_down), do: 0x09
-  def encode_thermostat_fan_mode(:quiet), do: 0x0A
+  @spec encode_thermostat_fan_mode(thermostat_fan_mode) ::
+          {:ok, fan_mode_byte} | {:error, :invalid_arg, any()}
+  def encode_thermostat_fan_mode(:auto_low), do: {:ok, 0x00}
+  def encode_thermostat_fan_mode(:low), do: {:ok, 0x01}
+  def encode_thermostat_fan_mode(:auto_high), do: {:ok, 0x02}
+  def encode_thermostat_fan_mode(:high), do: {:ok, 0x03}
+  def encode_thermostat_fan_mode(:auto_medium), do: {:ok, 0x04}
+  def encode_thermostat_fan_mode(:medium), do: {:ok, 0x05}
+  def encode_thermostat_fan_mode(:circulation), do: {:ok, 0x06}
+  def encode_thermostat_fan_mode(:humidity_circulation), do: {:ok, 0x07}
+  def encode_thermostat_fan_mode(:left_right), do: {:ok, 0x08}
+  def encode_thermostat_fan_mode(:up_down), do: {:ok, 0x09}
+  def encode_thermostat_fan_mode(:quiet), do: {:ok, 0x0A}
+  def encode_thermostat_fan_mode(other), do: {:error, :invalid_arg, other}
 
   @spec decode_thermostat_fan_mode(fan_mode_byte) :: thermostat_fan_mode
   def decode_thermostat_fan_mode(0x00), do: :auto_low
