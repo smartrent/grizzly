54f04e67055fa02e825b72ae8068d1590b68744f
jf@collaboration-planners.com <jean.f.cloutier@gmail.com>
2019-09-23 19:28:32 -0400

    Encodes errors for cc SwitchMultilevel. Partially fixes SRHUB-100

--- lib/grizzly/command_class/switch_multilevel.ex
+++ lib/grizzly/command_class/switch_multilevel.ex
@@ -1,10 +1,11 @@
 defmodule Grizzly.CommandClass.SwitchMultilevel do
   @type switch_state :: :previous | :off | byte()
 
-  @spec encode_switch_state(switch_state()) :: byte()
-  def encode_switch_state(:previous), do: 0xFF
-  def encode_switch_state(:off), do: 0x00
-  def encode_switch_state(value) when value in 0..99, do: value
+  @spec encode_switch_state(switch_state()) :: {:ok, byte()} | {:error, :invalid_arg, any()}
+  def encode_switch_state(:previous), do: {:ok, 0xFF}
+  def encode_switch_state(:off), do: {:ok, 0x00}
+  def encode_switch_state(value) when value in 0..99, do: {:ok, value}
+  def encode_switch_state(value), do: {:error, :invalid_arg, value}
 
   def decode_switch_state(0x00), do: :off
   def decode_switch_state(0xFE), do: :unknown
