bfe03ea8b9dc08bb5ce3e6e009b796450830ffde
jf@collaboration-planners.com <jean.f.cloutier@gmail.com>
2019-10-07 13:40:21 -0400

    Completing Association CC Fixes SRHUB-133

--- lib/grizzly/command_class/association.ex
+++ lib/grizzly/command_class/association.ex
@@ -2,4 +2,9 @@ defmodule Grizzly.CommandClass.Association do
   alias Grizzly.Node
 
   @type associated_nodes :: [Node.node_id()]
+
+  @spec decode_nodes(binary) :: associated_nodes
+  def decode_nodes(binary) do
+    :erlang.binary_to_list(binary)
+  end
 end
