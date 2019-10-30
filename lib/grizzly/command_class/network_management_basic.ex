ed5f4d7fb88b63cb3768ff044ff8c8796009c3e4
Matt Ludwigs <mattludwigs@gmail.com>
2019-10-30 11:34:54 -0700

    Support DSKGet command
    `DSKGet` get takes a `add_mode` argument because the controller can
    return two different types of DSKs, one for when the device is in learn
    mode and another for when the device is doing regular inclusion.

--- lib/grizzly/command_class/network_management_basic.ex
+++ lib/grizzly/command_class/network_management_basic.ex
@@ -3,6 +3,13 @@ defmodule Grizzly.CommandClass.NetworkManagementBasic do
   @type learn_mode_status :: :done | :failed | :failed_security
   @type learn_mode :: :enable | :disable | :enable_routed
   @type learn_mode_byte :: 0x00 | 0x01 | 0x02
+  @type add_mode :: :learn | :add
+  @type add_mode_byte :: 0x00 | 0x01
+
+  @type dsk_get_report :: %{
+          add_mode: add_mode(),
+          dsk: binary()
+        }
 
   @spec encode_learn_mode(learn_mode) :: {:ok, learn_mode_byte} | {:error, :invalid_arg, any()}
   def encode_learn_mode(mode) do
@@ -14,6 +21,30 @@ defmodule Grizzly.CommandClass.NetworkManagementBasic do
       # ZW_SET_LEARN_MODE_NWI - accept routed inclusion
       :enable_routed -> {:ok, 0x02}
       other -> {:error, :invalid_arg, other}
+    end
+  end
+
+  @doc """
+  Encode the add mode `:learn` or `:add` into a byte
+  """
+  @spec encode_add_mode(add_mode()) :: {:ok, add_mode_byte()} | {:error, :invalid_arg, any()}
+  def encode_add_mode(:learn), do: {:ok, 0x00}
+  def encode_add_mode(:add), do: {:ok, 0x01}
+  def encode_add_mode(other), do: {:error, :invalid_arg, other}
+
+  @doc """
+  Take the byte of the add mode and decode it into `:learn` or `:add`.
+  """
+  @spec add_mode_from_byte(byte()) :: add_mode()
+  def add_mode_from_byte(byte) do
+    <<_::size(7), add_mode_bit::size(1)>> = <<byte>>
+
+    case add_mode_bit do
+      0 ->
+        :learn
+
+      1 ->
+        :add
     end
   end
 
@@ -35,5 +66,23 @@ defmodule Grizzly.CommandClass.NetworkManagementBasic do
 
   def decode_learn_mode_set_status(0x09, _new_node_id) do
     %{status: :security_failed, new_node_id: 0}
+  end
+
+  @doc """
+  Decode the bytestring that is returned from Z-Wave that has the DSK
+  report
+  """
+  @spec decode_dsk_report(binary()) :: dsk_get_report()
+  def decode_dsk_report(<<add_mode, dsk::binary-size(16)>>) do
+    %{
+      add_mode: add_mode_from_byte(add_mode),
+      dsk: dsk_to_string(dsk)
+    }
+  end
+
+  defp dsk_to_string(binary) do
+    for(<<b::16 <- binary>>, do: b)
+    |> Enum.map(fn b -> String.slice("00000" <> "#{b}", -5, 5) end)
+    |> Enum.join("-")
   end
 end
