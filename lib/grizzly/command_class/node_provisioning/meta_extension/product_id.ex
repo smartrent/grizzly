3761c5e5767442e3e581bb60365e23b0146f5f5c
Matt Ludwigs <mattludwigs@gmail.com>
2019-11-13 16:01:19 -0700

    Add ProductId node provisioning meta extension

---
+++ lib/grizzly/command_class/node_provisioning/meta_extension/product_id.ex
@@ -0,0 +1,78 @@
+defmodule Grizzly.CommandClass.NodeProvisioning.MetaExtension.ProductId do
+  @moduledoc """
+  This Information Type is used to advertise the product identifying data of a supporting node.
+  """
+
+  @type u16 :: 0x0000..0xFFFF
+
+  @type param :: :manufacturer_id | :product_type | :product_id | :application_version
+
+  @type t :: %__MODULE__{
+          manufacturer_id: u16(),
+          product_id: u16(),
+          product_type: u16(),
+          application_version: {byte(), byte()}
+        }
+
+  defstruct manufacturer_id: nil, product_type: nil, product_id: nil, application_version: nil
+
+  defguardp is_u16(value) when value in 0x0000..0xFFFF
+
+  @doc """
+  Make a `ProductId.t()` from a binary string
+
+  According to the specification for this extension the critical bit must not
+  be set. If it is then the binary is not consider valid and should be
+  ignored.
+  """
+  @spec from_binary(binary()) :: {:ok, t()} | {:error, :critical_bit_set}
+  def from_binary(
+        <<1::size(7), 0::size(1), 0x08, manufacturer_id::size(16), product_type::size(16),
+          product_id::size(16), application_version, application_sub_version>>
+      ) do
+    {:ok,
+     %__MODULE__{
+       manufacturer_id: manufacturer_id,
+       product_id: product_id,
+       product_type: product_type,
+       application_version: {application_version, application_sub_version}
+     }}
+  end
+
+  def from_binary(<<1::size(7), 1::size(1), _rest::binary>>) do
+    {:error, :critical_bit_set}
+  end
+
+  @doc """
+  Build a binary from a `ProductId.t()`
+
+  The arguments for the `params()` other than application version should be
+  unsigned 16 bit integers.
+  """
+  @spec to_binary(t()) ::
+          {:ok, binary()} | {:error, :invalid_param_argument, param(), value :: any()}
+  def to_binary(%__MODULE__{
+        manufacturer_id: manufacturer_id,
+        product_type: product_type,
+        product_id: product_id,
+        application_version: application_version
+      }) do
+    with :ok <- validate_param(:manufacturer_id, manufacturer_id),
+         :ok <- validate_param(:product_id, product_id),
+         :ok <- validate_param(:product_type, product_type),
+         :ok <- validate_param(:application_version, application_version) do
+      {:ok,
+       <<0x02, 0x08, manufacturer_id::size(16), product_type::size(16), product_id::size(16),
+         elem(application_version, 0), elem(application_version, 1)>>}
+    end
+  end
+
+  defp validate_param(:manufacturer_id, mid) when is_u16(mid), do: :ok
+  defp validate_param(:product_type, pt) when is_u16(pt), do: :ok
+  defp validate_param(:product_id, pid) when is_u16(pid), do: :ok
+
+  defp validate_param(:application_version, {v1, v2}) when v1 in 0x00..0xFF and v2 in 0x00..0xFF,
+    do: :ok
+
+  defp validate_param(param, value), do: {:error, :invalid_param_argument, param, value}
+end
