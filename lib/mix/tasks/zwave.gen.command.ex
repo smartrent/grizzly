76a6790178621b3d7d242185f4eb20ccf2a29545
Matt Ludwigs <mattludwigs@gmail.com>
2020-04-17 09:23:58 -0700

    Generate command test when using mix task for command

--- lib/mix/tasks/zwave.gen.command.ex
+++ lib/mix/tasks/zwave.gen.command.ex
@@ -12,29 +12,25 @@ defmodule Mix.Tasks.Zwave.Gen.Command do
 
     * `--no-save` - will print the generated module out to the console
     * `--target-dir <path>` - will generate the new module at the provided path
+    * `--no_test` - if set, will not generate a test file
   """
 
   use Mix.Task
 
   @shortdoc "Generates the scaffolding for a new Z-Wave Command"
 
-  @switches [no_save: :boolean, target_dir: :string]
+  @switches [no_save: :boolean, target_dir: :string, no_test: :boolean]
 
   def run(args) do
     {opts, [command_name, command_class | params], _} =
       OptionParser.parse(args, switches: @switches)
 
-    command_name_str_snake_case = Macro.underscore(command_name)
+    target_defs = build_target_defs(command_name, opts)
 
-    target_path = Path.join(get_target(opts), "#{command_name_str_snake_case}.ex")
-
-    copy_files(target_path, command_name, command_class, params, opts)
+    copy_files(target_defs, command_name, command_class, params, opts)
   end
 
-  defp copy_files(target_path, command_name, command_class, params, opts) do
-    template_file =
-      Path.join(Application.app_dir(:grizzly), "/priv/templates/zwave.gen/command.ex")
-
+  defp copy_files(target_defs, command_name, command_class, params, opts) do
     command_module = Module.concat(Grizzly.ZWave.Commands, command_name)
     command_name = String.to_atom(Macro.underscore(command_name))
     command_class_module = Module.concat(Grizzly.ZWave.CommandClasses, command_class)
@@ -46,20 +42,32 @@ defmodule Mix.Tasks.Zwave.Gen.Command do
       command_class_module: command_class_module
     ]
 
-    to_out(target_path, template_file, bindings, opts)
+    Enum.each(target_defs, fn target_def ->
+      to_out(target_def, bindings, opts)
+    end)
   end
 
-  defp to_out(target_path, template_file, bindings, opts) do
+  defp to_out({:target, target_file, template_file}, bindings, opts) do
     evaled = EEx.eval_file(template_file, bindings)
 
     if Keyword.has_key?(opts, :no_save) do
       IO.puts(evaled)
     else
-      Mix.Generator.create_file(target_path, EEx.eval_file(template_file, bindings))
+      Mix.Generator.create_file(target_file, evaled)
+    end
+
+    :ok
+  end
+
+  defp to_out({:target_test, test_file, test_template_file}, bindings, opts) do
+    if Keyword.has_key?(opts, :no_test) || Keyword.has_key?(opts, :no_save) do
+      :ok
+    else
+      Mix.Generator.create_file(test_file, EEx.eval_file(test_template_file, bindings))
     end
   end
 
-  def get_target(opts) do
+  defp get_target(file_name, opts) do
     case Keyword.get(opts, :target_dir) do
       nil ->
         Path.join([
@@ -67,11 +75,43 @@ defmodule Mix.Tasks.Zwave.Gen.Command do
           "lib",
           "grizzly",
           "zwave",
-          "commands"
+          "commands",
+          "#{file_name}.ex"
         ])
 
       target_dir ->
-        target_dir
+        Path.join([File.cwd!(), "lib", target_dir, "#{file_name}.ex"])
     end
+  end
+
+  defp get_test_target(file_name, opts) do
+    case Keyword.get(opts, :target_dir) do
+      nil ->
+        Path.join([
+          File.cwd!(),
+          "test",
+          "grizzly",
+          "zwave",
+          "commands",
+          "#{file_name}_test.exs"
+        ])
+
+      target_dir ->
+        Path.join([File.cwd!(), "test", target_dir, "#{file_name}_test.exs"])
+    end
+  end
+
+  defp build_target_defs(command_name, opts) do
+    command_name_str = Macro.underscore(command_name)
+    grizzly_path = Application.app_dir(:grizzly)
+    target = get_target(command_name_str, opts)
+    test_target = get_test_target(command_name_str, opts)
+    target_template = Path.join(grizzly_path, "/priv/templates/zwave.gen/command.ex")
+    test_template = Path.join(grizzly_path, "/priv/templates/zwave.gen/command_test.exs")
+
+    [
+      {:target, target, target_template},
+      {:target_test, test_target, test_template}
+    ]
   end
 end
