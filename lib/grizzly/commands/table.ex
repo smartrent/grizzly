dfaf269fb51f8158af6d72c7b8039ba2e760d2b9
Matt Ludwigs <mattludwigs@gmail.com>
2020-04-17 09:56:53 -0700

    Add more code gen

--- lib/grizzly/commands/table.ex
+++ lib/grizzly/commands/table.ex
@@ -5,52 +5,60 @@ defmodule Grizzly.Commands.Table do
   # runtime related options. This where Grizzly and Z-Wave meet in regards to
   # out going commands
 
-  alias Grizzly.CommandHandlers.{AckResponse, WaitReport, AggregateReport}
+  defmodule Generate do
+    @moduledoc false
+    alias Grizzly.CommandHandlers.{AckResponse, WaitReport, AggregateReport}
 
-  alias Grizzly.ZWave.Commands
+    alias Grizzly.ZWave.Commands
 
-  @doc """
-  Look up the Z-Wave command module and default Grizzly command options via the
-  command name
-  """
-  @spec lookup(Grizzly.command()) :: {module(), [Grizzly.command_opt()]}
-  def lookup(:default_set),
-    do: {Commands.DefaultSet, handler: {WaitReport, complete_report: :default_set_complete}}
+    @table [
+      {:default_set,
+       {Commands.DefaultSet, handler: {WaitReport, complete_report: :default_set_complete}}},
+      {:switch_binary_get,
+       {Commands.SwitchBinaryGet, handler: {WaitReport, complete_report: :switch_binary_report}}},
+      {:switch_binary_set, {Commands.SwitchBinarySet, handler: AckResponse}},
+      {:node_list_get,
+       {Commands.NodeListGet, handler: {WaitReport, complete_report: :node_list_report}}},
+      {:node_add, {Commands.NodeAdd, handler: {WaitReport, complete_report: :node_add_status}}},
+      {:node_info_cached_get,
+       {Commands.NodeInfoCachedGet,
+        handler: {WaitReport, complete_report: :node_info_cache_report}}},
+      {:node_remove,
+       {Commands.NodeRemove, handler: {WaitReport, complete_report: :node_remove_status}}},
+      {:node_add_keys_set, {Commands.NodeAddKeysSet, handler: AckResponse}},
+      {:node_add_dsk_set, {Commands.NodeAddDSKSet, handler: AckResponse}},
+      {:association_set, do: {Commands.AssociationSet, handler: AckResponse}},
+      {:association_get,
+       {Commands.AssociationGet,
+        handler: {AggregateReport, complete_report: :association_report, aggregate_field: :nodes}}}
+    ]
 
-  def lookup(:switch_binary_get),
-    do: {Commands.SwitchBinaryGet, handler: {WaitReport, complete_report: :switch_binary_report}}
+    defmacro __before_compile__(_) do
+      lookup =
+        for {command_class, spec} <- @table do
+          quote location: :keep do
+            def lookup(unquote(command_class)), do: unquote(spec)
+          end
+        end
 
-  def lookup(:switch_binary_set), do: {Commands.SwitchBinarySet, handler: AckResponse}
+      quote location: :keep do
+        @doc """
+        Look up the Z-Wave command module and default Grizzly command options via the
+        command name
+        """
+        @spec lookup(Grizzly.command()) :: {module(), [Grizzly.command_opt()]}
+        unquote(lookup)
 
-  def lookup(:node_list_get),
-    do: {Commands.NodeListGet, handler: {WaitReport, complete_report: :node_list_report}}
-
-  def lookup(:node_add),
-    do: {Commands.NodeAdd, handler: {WaitReport, complete_report: :node_add_status}}
-
-  def lookup(:node_info_cached_get),
-    do:
-      {Commands.NodeInfoCachedGet,
-       handler: {WaitReport, complete_report: :node_info_cache_report}}
-
-  def lookup(:node_remove),
-    do: {Commands.NodeRemove, handler: {WaitReport, complete_report: :node_remove_status}}
-
-  def lookup(:node_add_keys_set), do: {Commands.NodeAddKeysSet, handler: AckResponse}
-  def lookup(:node_add_dsk_set), do: {Commands.NodeAddDSKSet, handler: AckResponse}
-
-  def lookup(:association_set), do: {Commands.AssociationSet, handler: AckResponse}
-
-  def lookup(:association_get),
-    do:
-      {Commands.AssociationGet,
-       handler: {AggregateReport, complete_report: :association_report, aggregate_field: :nodes}}
-
-  def lookup(_) do
-    raise ArgumentError, """
-    The command you are trying to send is not supported
-    """
+        def lookup(_) do
+          raise ArgumentError, """
+          The command you are trying to send is not supported
+          """
+        end
+      end
+    end
   end
+
+  @before_compile Generate
 
   @doc """
   Get the handler spec for the command
--- lib/grizzly/zwave/decoder.ex
+++ lib/grizzly/zwave/decoder.ex
@@ -1,49 +1,62 @@
 defmodule Grizzly.ZWave.Decoder do
   @moduledoc false
 
-  alias Grizzly.ZWave.{Command, Commands, DecodeError}
+  defmodule Generate do
+    @moduledoc false
+    alias Grizzly.ZWave.{Command, Commands, DecodeError}
 
-  @spec from_binary(binary) :: {:ok, Command.t()} | {:error, DecodeError.t()}
-  # Switch Binary (0x25)
-  def from_binary(<<0x25, 0x01, params::binary>>),
-    do: decode(Commands.SwitchBinarySet, params)
+    @mappings [
+      # {command_class_byte, command_byte, command_module}
+      # Switch Binary (0x25)
+      {0x25, 0x01, Commands.SwitchBinarySet},
+      {0x25, 0x02, Commands.SwitchBinaryGet},
+      {0x25, 0x03, Commands.SwitchBinaryReport},
+      # Network Management Inclusion (0x34)
+      {0x34, 0x01, Commands.NodeAdd},
+      {0x34, 0x02, Commands.NodeAddStatus},
+      {0x34, 0x03, Commands.NodeRemove},
+      {0x34, 0x04, Commands.NodeRemoveStatus},
+      {0x34, 0x11, Commands.NodeAddKeysReport},
+      {0x34, 0x12, Commands.NodeAddKeysSet},
+      {0x34, 0x13, Commands.NodeAddDSKReport},
+      {0x34, 0x14, Commands.NodeAddDSKSet},
+      # Network Management Basic Node (0x4D)
+      {0x4D, 0x07, Commands.DefaultSetComplete},
+      # Network Management Proxy (0x52)
+      {0x52, 0x01, Commands.NodeListGet},
+      {0x52, 0x02, Commands.NodeListReport},
+      {0x52, 0x04, Commands.NodeInfoCacheReport},
+      # Association (0x85)
+      {0x85, 0x03, Commands.AssociationReport}
+    ]
 
-  def from_binary(<<0x25, 0x02>>),
-    do: decode(Commands.SwitchBinaryGet, [])
+    defmacro __before_compile__(_) do
+      from_binary =
+        for {command_class_byte, command_byte, command_module} <- @mappings do
+          quote do
+            def from_binary(
+                  <<unquote(command_class_byte), unquote(command_byte), params::binary>>
+                ),
+                do: decode(unquote(command_module), params)
+          end
+        end
 
-  def from_binary(<<0x25, 0x03, params::binary>>),
-    do: decode(Commands.SwitchBinaryReport, params)
+      quote do
+        @spec from_binary(binary) :: {:ok, Command.t()} | {:error, DecodeError.t()}
+        unquote(from_binary)
 
-  # Network Management Inclusion (0x34)
-  def from_binary(<<0x34, 0x01, params::binary>>), do: decode(Commands.NodeAdd, params)
-  def from_binary(<<0x34, 0x02, params::binary>>), do: decode(Commands.NodeAddStatus, params)
-  def from_binary(<<0x34, 0x03, params::binary>>), do: decode(Commands.NodeRemove, params)
-  def from_binary(<<0x34, 0x04, params::binary>>), do: decode(Commands.NodeRemoveStatus, params)
-  def from_binary(<<0x34, 0x11, params::binary>>), do: decode(Commands.NodeAddKeysReport, params)
-  def from_binary(<<0x34, 0x12, params::binary>>), do: decode(Commands.NodeAddKeysSet, params)
-  def from_binary(<<0x34, 0x13, params::binary>>), do: decode(Commands.NodeAddDSKReport, params)
-  def from_binary(<<0x34, 0x14, params::binary>>), do: decode(Commands.NodeAddDSKSet, params)
+        defp decode(command_impl, params) do
+          case command_impl.decode_params(params) do
+            {:ok, decoded_params} ->
+              command_impl.new(decoded_params)
 
-  # Network Management Basic Node (0x4D)
-  def from_binary(<<0x4D, 0x07, params::binary>>), do: decode(Commands.DefaultSetComplete, params)
-
-  # Network Management Proxy (0x52)
-  def from_binary(<<0x52, 0x01, params::binary>>), do: decode(Commands.NodeListGet, params)
-  def from_binary(<<0x52, 0x02, params::binary>>), do: decode(Commands.NodeListReport, params)
-
-  def from_binary(<<0x52, 0x04, params::binary>>),
-    do: decode(Commands.NodeInfoCacheReport, params)
-
-  # Association (0x85)
-  def from_binary(<<0x85, 0x03, params::binary>>), do: decode(Commands.AssociationReport, params)
-
-  defp decode(command_impl, params) do
-    case command_impl.decode_params(params) do
-      {:ok, decoded_params} ->
-        command_impl.new(decoded_params)
-
-      {:error, %DecodeError{}} = error ->
-        error
+            {:error, %DecodeError{}} = error ->
+              error
+          end
+        end
+      end
     end
   end
+
+  @before_compile Generate
 end
