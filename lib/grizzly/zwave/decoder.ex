40f03de77f296e8bb737b70f26b5b1648bc5cff3
Jean-Francois Cloutier <jean.f.cloutier@gmail.com>
2020-05-06 13:10:43 -0400

    Misc fixes

--- lib/grizzly/zwave/decoder.ex
+++ lib/grizzly/zwave/decoder.ex
@@ -54,8 +54,8 @@ defmodule Grizzly.ZWave.Decoder do
       {0x86, 0x13, Commands.CommandClassGet},
       {0x86, 0x14, Commands.CommandClassReport},
       # Firmware Update Metadata
-      {0x7A, 0x01, Commands, FirmwareUpdateMDGet},
-      {0x7A, 0x02, Commands, FirmwareMDReport}
+      {0x7A, 0x01, Commands.FirmwareUpdateMDGet},
+      {0x7A, 0x02, Commands.FirmwareMDReport}
     ]
 
     defmacro __before_compile__(_) do
