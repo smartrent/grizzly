eee828141e970452a42086dd0f6801bad561c02f
Matt Ludwigs <mattludwigs@gmail.com>
2020-05-11 13:40:09 -0700

    Add a lifeline helper function

--- lib/grizzly/node.ex
+++ lib/grizzly/node.ex
@@ -4,15 +4,18 @@ defmodule Grizzly.Node do
   """
 
   alias Grizzly.SeqNumber
+  alias Grizzly.ZWave
 
   @type id :: non_neg_integer()
+
+  @type lifeline_opts :: {:controller_id, ZWave.node_id()} | {:extra_node_ids, [ZWave.node_id()]}
 
   @doc """
   Get the information for a node by its id
 
   The response to this command is the `NodeInfoCacheReport` command
   """
-  @spec get_node_info(id()) :: Grizzly.send_command_response()
+  @spec get_node_info(ZWave.node_id()) :: Grizzly.send_command_response()
   def get_node_info(node_id) do
     seq_number = SeqNumber.get_and_inc()
 
@@ -27,7 +30,8 @@ defmodule Grizzly.Node do
 
   The response to this command is the `DSKReport` command
   """
-  @spec get_dsk(id(), :add | :learn, [Grizzly.command_opt()]) :: Grizzly.send_command_response()
+  @spec get_dsk(ZWave.node_id(), :add | :learn, [Grizzly.command_opt()]) ::
+          Grizzly.send_command_response()
   def get_dsk(node_id, add_mode, opts \\ []) do
     Grizzly.send_command(
       node_id,
@@ -35,5 +39,29 @@ defmodule Grizzly.Node do
       [add_mode: add_mode, seq_number: SeqNumber.get_and_inc()],
       opts
     )
+  end
+
+  @doc """
+  Set lifeline association for the node
+
+  The lifeline associated is used to report events that happen at the Z-Wave
+  device level back to the Z-Wave controller. For example, if you turn on a
+  light and want to be notified, the device will need to have it's lifeline
+  association set.
+
+  Opts:
+
+    * `:controller_id` - the idea of the main controller to send lifeline
+      reports to (defaults to `1`)
+    * `:extra_node_ids` - any extra nodes to set add to the association
+      group
+  """
+  @spec set_lifeline_association(ZWave.node_id(), [lifeline_opts()]) :: :ok
+  def set_lifeline_association(node_id, opts \\ []) do
+    controller_id = Keyword.get(opts, :controller_id, 1)
+    extra_node_ids = Keyword.get(opts, :extra_node_ids, [])
+    nodes = [controller_id | extra_node_ids]
+
+    Grizzly.send_command(node_id, :association_set, grouping_identifier: 0x01, nodes: nodes)
   end
 end
