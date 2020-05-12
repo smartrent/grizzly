b172e6b1896b18b3cccd3845b124cfb7edec0b55
Jean-Francois Cloutier <jean.f.cloutier@gmail.com>
2020-05-12 11:07:04 -0400

    Added water and siren to Notifications table

--- lib/grizzly/zwave/notifications.ex
+++ lib/grizzly/zwave/notifications.ex
@@ -13,6 +13,20 @@ defmodule Grizzly.ZWave.Notifications do
       {0x01, :smoke_alarm, 0x06, :alarm_silenced},
       {0x01, :smoke_alarm, 0x07, :maintenance_required_planned_periodic_inspection},
       {0x01, :smoke_alarm, 0x08, :maintenance_required_dust_in_device},
+      # Water (0x05)
+      {0x05, :water, 0x00, :state_idle},
+      {0x05, :water, 0x01, :water_leak_detected_known_location},
+      {0x05, :water, 0x02, :water_leak_detected},
+      {0x05, :water, 0x03, :water_level_dropped_known_location},
+      {0x05, :water, 0x04, :water_level_dropped},
+      {0x05, :water, 0x05, :replace_water_filter},
+      {0x05, :water, 0x06, :water_flow_alarm},
+      {0x05, :water, 0x07, :water_pressure_alarm},
+      {0x05, :water, 0x08, :water_temperature_alarm},
+      {0x05, :water, 0x09, :water_level_alarm},
+      {0x05, :water, 0x0A, :sump_pump_active},
+      {0x05, :water, 0x0B, :sump_pump_failure},
+      {0x05, :water, 0xFE, :unknown},
       # Access Control (0x06)
       {0x06, :access_control, 0x00, :state_idle},
       {0x06, :access_control, 0x01, :manual_lock_operation},
@@ -32,7 +46,10 @@ defmodule Grizzly.ZWave.Notifications do
       {0x07, :home_security, 0x09, :tampering_product_moved},
       {0x07, :home_security, 0x0A, :impact_detected},
       {0x07, :home_security, 0x0B, :magnetic_field_interface_detected},
-      {0x07, :home_security, 0xFE, :unknown_state}
+      {0x07, :home_security, 0xFE, :unknown_state},
+      # Siren (0x0E)
+      {0x0E, :siren, 0x00, :state_idle},
+      {0x0E, :siren, 0x01, :state_active}
     ]
 
     defmacro __before_compile__(_) do
