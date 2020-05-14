777fc5224ab90401a368a6fd752b7c009f2d1cbd
Jean-Francois Cloutier <jean.f.cloutier@gmail.com>
2020-05-14 16:54:58 -0400

    Fixed typo in registering UserCodeUsersNumberGet

--- lib/grizzly/commands/table.ex
+++ lib/grizzly/commands/table.ex
@@ -97,7 +97,7 @@ defmodule Grizzly.Commands.Table do
       {:user_code_get,
        {Commands.UserCodeGet, handler: {WaitReport, complete_report: :user_code_report}}},
       {:user_code_users_number_get,
-       {Commands.UserCodeUSersNumberGet,
+       {Commands.UserCodeUsersNumberGet,
         handler: {WaitReport, complete_report: :user_code_users_number_report}}},
       # Meter
       {:meter_get, {Commands.MeterGet, handler: {WaitReport, complete_report: :meter_report}}},
