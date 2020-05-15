fdc81f6cea15912dcac2ffd13bd7891578fabaa0
Jean-Francois Cloutier <jean.f.cloutier@gmail.com>
2020-05-15 15:32:14 -0400

    Allowing for default seq_numbers

--- lib/grizzly/network.ex
+++ lib/grizzly/network.ex
@@ -18,4 +18,48 @@ defmodule Grizzly.Network do
 
     Grizzly.send_command(1, :default_set, [seq_number: seq_number], timeout: 10_000)
   end
+
+  @spec delete_node_provisioning(Grizzly.ZWave.DSK.dsk_string()) ::
+          Grizzly.send_command_response()
+  def delete_node_provisioning(dsk) do
+    seq_number = SeqNumber.get_and_inc()
+
+    Grizzly.send_command(1, :node_provisioning_delete, seq_number: seq_number, dsk: dsk)
+  end
+
+  @spec get_node_provisioning(Grizzly.ZWave.DSK.dsk_string()) ::
+          Grizzly.send_command_response()
+  def get_node_provisioning(dsk) do
+    seq_number = SeqNumber.get_and_inc()
+
+    Grizzly.send_command(1, :node_provisioning_get, seq_number: seq_number, dsk: dsk)
+  end
+
+  @spec set_node_provisioning(
+          Grizzly.ZWave.DSK.dsk_string(),
+          [Grizzly.ZWave.SmartStart.MetaExtension.t()]
+        ) :: Grizzly.send_command_response()
+  def set_node_provisioning(dsk, meta_extensions) do
+    seq_number = SeqNumber.get_and_inc()
+
+    Grizzly.send_command(
+      1,
+      :node_provisioning_set,
+      seq_number: seq_number,
+      dsk: dsk,
+      meta_extensions: meta_extensions
+    )
+  end
+
+  @spec list_node_provisionings(integer) :: Grizzly.send_command_response()
+  def list_node_provisionings(remaining_counter) do
+    seq_number = SeqNumber.get_and_inc()
+
+    Grizzly.send_command(
+      1,
+      :node_provisioning_list_iteration_get,
+      seq_number: seq_number,
+      remaining_counter: remaining_counter
+    )
+  end
 end
