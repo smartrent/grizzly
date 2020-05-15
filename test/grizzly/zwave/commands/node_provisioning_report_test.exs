4b7ab7601c3390116ae72d0b676210d3f2799f9b
Jean-Francois Cloutier <jean.f.cloutier@gmail.com>
2020-05-15 17:09:40 -0400

    Fixes to node provisioning

--- test/grizzly/zwave/commands/node_provisioning_report_test.exs
+++ test/grizzly/zwave/commands/node_provisioning_report_test.exs
@@ -28,6 +28,20 @@ defmodule Grizzly.ZWave.Commands.NodeProvisioningReportTest do
     assert expected_binary == NodeProvisioningReport.encode_params(command)
   end
 
+  test "encodes params correctly with empty dsk" do
+    params = [
+      seq_number: 0x01,
+      dsk: "",
+      meta_extensions: []
+    ]
+
+    {:ok, command} = NodeProvisioningReport.new(params)
+
+    expected_binary = <<0x01, 0x00>>
+
+    assert expected_binary == NodeProvisioningReport.encode_params(command)
+  end
+
   test "decodes params correctly" do
     binary_params =
       <<0x01, 0x10, 196, 109, 73, 131, 38, 196, 119, 227, 62, 101, 131, 175, 15, 165, 14, 39>>
@@ -35,6 +49,15 @@ defmodule Grizzly.ZWave.Commands.NodeProvisioningReportTest do
     {:ok, params} = NodeProvisioningReport.decode_params(binary_params)
     assert Keyword.get(params, :seq_number) == 1
     assert Keyword.get(params, :dsk) == "50285-18819-09924-30691-15973-33711-04005-03623"
+    assert Keyword.get(params, :meta_extensions) == []
+  end
+
+  test "decodes params correctly with empty dsk" do
+    binary_params = <<0x01, 0x00>>
+
+    {:ok, params} = NodeProvisioningReport.decode_params(binary_params)
+    assert Keyword.get(params, :seq_number) == 1
+    assert Keyword.get(params, :dsk) == ""
     assert Keyword.get(params, :meta_extensions) == []
   end
 end
