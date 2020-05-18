15ce0305d5add84cca24013ce858b7473aa49441
Jean-Francois Cloutier <jean.f.cloutier@gmail.com>
2020-05-18 16:35:28 -0400

    low battery warning

--- lib/grizzly/zwave/commands/battery_report.ex
+++ lib/grizzly/zwave/commands/battery_report.ex
@@ -146,6 +146,8 @@ defmodule Grizzly.ZWave.Commands.BatteryReport do
   defp encode_low_temperature(true), do: 0x01
 
   defp level_from_byte(level_byte) when level_byte in 0..100, do: {:ok, level_byte}
+  # low battery warning
+  defp level_from_byte(0xFF), do: {:ok, 0}
 
   defp level_from_byte(byte),
     do: {:error, %DecodeError{value: byte, param: :level, command: :battery_report}}
