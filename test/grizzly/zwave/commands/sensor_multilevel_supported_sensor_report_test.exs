441bfb009af7d3cdcc4251d70a85820f6e71ddef
Jean-Francois Cloutier <jean.f.cloutier@gmail.com>
2020-06-16 16:00:57 -0400

    Fixed supported sensors report

--- test/grizzly/zwave/commands/sensor_multilevel_supported_sensor_report_test.exs
+++ test/grizzly/zwave/commands/sensor_multilevel_supported_sensor_report_test.exs
@@ -4,19 +4,22 @@ defmodule Grizzly.ZWave.Commands.SensorMultilevelSupportedSensorReportTest do
   alias Grizzly.ZWave.Commands.SensorMultilevelSupportedSensorReport
 
   test "creates the command and validates params" do
-    params = [sensor_types: [:air_temperature, :luminance, :air_flow, :moisture]]
+    params = [
+      sensor_types: [:air_temperature, :luminance, :seismic_magnitude, :water_temperature]
+    ]
+
     {:ok, _command} = SensorMultilevelSupportedSensorReport.new(params)
   end
 
   test "encodes params correctly" do
-    params = [sensor_types: [:air_temperature, :luminance, :air_flow, :moisture]]
+    params = [sensor_types: [:air_temperature, :humidity, :seismic_magnitude, :water_temperature]]
     {:ok, command} = SensorMultilevelSupportedSensorReport.new(params)
-    expected_binary = <<160, 0, 64, 2>>
+    expected_binary = <<17, 0, 64, 2>>
     assert expected_binary == SensorMultilevelSupportedSensorReport.encode_params(command)
   end
 
   test "decodes params correctly" do
-    binary_params = <<160, 0, 64, 2>>
+    binary_params = <<17, 0, 64, 2>>
     {:ok, params} = SensorMultilevelSupportedSensorReport.decode_params(binary_params)
 
     sensor_types = Keyword.get(params, :sensor_types)
@@ -24,9 +27,9 @@ defmodule Grizzly.ZWave.Commands.SensorMultilevelSupportedSensorReportTest do
     assert Enum.sort(sensor_types) ==
              Enum.sort([
                :air_temperature,
-               :luminance,
-               :air_flow,
-               :moisture
+               :humidity,
+               :seismic_magnitude,
+               :water_temperature
              ])
   end
 end
