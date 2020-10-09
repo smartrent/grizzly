e453e649979e987cac8b2e4b66a9994f06a4c5f0
Matt Ludwigs <mattludwigs@users.noreply.github.com>
2020-10-09 09:54:17 -0700

    Fix V2 support for SwitchBinary Command Class

---
+++ test/grizzly/zwave/commands/switch_binary_report_test.exs
@@ -0,0 +1,95 @@
+defmodule Grizzly.ZWave.Commands.SwitchBinaryReportTest do
+  use ExUnit.Case, async: true
+
+  alias Grizzly.ZWave
+  alias Grizzly.ZWave.Commands.SwitchBinaryReport
+
+  describe "Support V1" do
+    test "encodes correctly on" do
+      {:ok, report} = SwitchBinaryReport.new(target_value: :on)
+
+      assert <<0x25, 0x03, 0xFF>> == ZWave.to_binary(report)
+    end
+
+    test "encodes correctly off" do
+      {:ok, report} = SwitchBinaryReport.new(target_value: :off)
+
+      assert <<0x25, 0x03, 0x00>> == ZWave.to_binary(report)
+    end
+
+    test "encodes correctly unknown" do
+      {:ok, report} = SwitchBinaryReport.new(target_value: :unknown)
+
+      assert <<0x25, 0x03, 0xFE>> == ZWave.to_binary(report)
+    end
+
+    test "decodes correctly on" do
+      binary = <<0x25, 0x03, 0xFF>>
+      expected_report = SwitchBinaryReport.new(target_value: :on)
+
+      assert expected_report == ZWave.from_binary(binary)
+    end
+
+    test "decodes correctly off" do
+      binary = <<0x25, 0x03, 0x00>>
+      expected_report = SwitchBinaryReport.new(target_value: :off)
+
+      assert expected_report == ZWave.from_binary(binary)
+    end
+
+    test "decodes correctly unknown" do
+      binary = <<0x25, 0x03, 0xFE>>
+      expected_report = SwitchBinaryReport.new(target_value: :unknown)
+
+      assert expected_report == ZWave.from_binary(binary)
+    end
+  end
+
+  describe "Support V2" do
+    test "encodes correctly on" do
+      {:ok, report} = SwitchBinaryReport.new(target_value: :on, duration: 0, current_value: :off)
+
+      assert <<0x25, 0x03, 0x00, 0xFF, 0x00>> == ZWave.to_binary(report)
+    end
+
+    test "encodes correctly off" do
+      {:ok, report} = SwitchBinaryReport.new(target_value: :off, duration: 0, current_value: :on)
+
+      assert <<0x25, 0x03, 0xFF, 0x00, 0x00>> == ZWave.to_binary(report)
+    end
+
+    test "encodes correctly unknown" do
+      {:ok, report} =
+        SwitchBinaryReport.new(target_value: :unknown, duration: 0, current_value: :off)
+
+      assert <<0x25, 0x03, 0x00, 0xFE, 0x00>> == ZWave.to_binary(report)
+    end
+
+    test "decodes correctly on" do
+      binary = <<0x25, 0x03, 0x00, 0xFF, 0x00>>
+
+      expected_report =
+        SwitchBinaryReport.new(target_value: :on, duration: 0, current_value: :off)
+
+      assert expected_report == ZWave.from_binary(binary)
+    end
+
+    test "decodes correctly off" do
+      binary = <<0x25, 0x03, 0xFF, 0x00, 0x00>>
+
+      expected_report =
+        SwitchBinaryReport.new(target_value: :off, duration: 0x00, current_value: :on)
+
+      assert expected_report == ZWave.from_binary(binary)
+    end
+
+    test "decodes correctly unknown" do
+      binary = <<0x25, 0x03, 0xFE, 0xFE, 0x00>>
+
+      expected_report =
+        SwitchBinaryReport.new(target_value: :unknown, duration: 0x00, current_value: :unknown)
+
+      assert expected_report == ZWave.from_binary(binary)
+    end
+  end
+end
