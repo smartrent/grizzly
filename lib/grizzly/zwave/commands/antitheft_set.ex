d88a6e1240223adc1d4109a771ca8bcd5d107532
Jean-Francois Cloutier <jean.f.cloutier@gmail.com>
2021-01-07 15:34:27 -0500

    Antitheft CC v2

--- lib/grizzly/zwave/commands/antitheft_set.ex
+++ lib/grizzly/zwave/commands/antitheft_set.ex
@@ -4,15 +4,15 @@ defmodule Grizzly.ZWave.Commands.AntitheftSet do
 
   Params:
 
-    * `:state` - This field MUST indicate the desired :locked or :unlocked state for the receiving node (required)
+    * `:state` - This field MUST indicate the desired :locked or :unlocked state for the receiving node (required v2+)
 
-    * `:magic_code` - This field contains the 1 to 10 byte Magic Code used to lock or unlock the node (required)
+    * `:magic_code` - This field contains the 1 to 10 byte Magic Code used to lock or unlock the node (required v2+)
 
-    * `:manufacturer_id` - This field describes the Z-Wave Manufacturer ID of the company’s product that has locked the node (required)
+    * `:manufacturer_id` - This field describes the Z-Wave Manufacturer ID of the company’s product that has locked the node (required v2+)
 
-    * `:antitheft_hint` - This field is used as a 1 to 10 byte identifier or key value to help retriving the Magic Code (required)
+    * `:antitheft_hint` - This field is used as a 1 to 10 byte identifier or key value to help retriving the Magic Code (required v2+)
 
-    * `:locking_entity_id` - This field MUST specify a unique Z-Wave Alliance identifier for the entity that has locked the node (required)
+    * `:locking_entity_id` - This field MUST specify a unique Z-Wave Alliance identifier for the entity that has locked the node (required v3)
 
   """
 
@@ -52,17 +52,25 @@ defmodule Grizzly.ZWave.Commands.AntitheftSet do
     antitheft_hint =
       Command.param!(command, :antitheft_hint) |> Antitheft.validate_magic_code_or_hint()
 
-    locking_entity_id = Command.param!(command, :locking_entity_id)
+    locking_entity_id = Command.param(command, :locking_entity_id)
 
-    <<state_bit::size(1), byte_size(magic_code)::size(7)>> <>
-      magic_code <>
-      <<manufacturer_id::size(16), byte_size(antitheft_hint)>> <>
-      antitheft_hint <>
-      <<locking_entity_id::size(16)>>
+    if locking_entity_id == nil do
+      <<state_bit::size(1), byte_size(magic_code)::size(7)>> <>
+        magic_code <>
+        <<manufacturer_id::size(16), byte_size(antitheft_hint)>> <>
+        antitheft_hint
+    else
+      <<state_bit::size(1), byte_size(magic_code)::size(7)>> <>
+        magic_code <>
+        <<manufacturer_id::size(16), byte_size(antitheft_hint)>> <>
+        antitheft_hint <>
+        <<locking_entity_id::size(16)>>
+    end
   end
 
   @impl true
   @spec decode_params(binary()) :: {:ok, [param()]} | {:error, DecodeError.t()}
+  # v3
   def decode_params(
         <<state_bit::size(1), magic_code_length::size(7),
           magic_code::binary-size(magic_code_length), manufacturer_id::size(16),
@@ -78,6 +86,23 @@ defmodule Grizzly.ZWave.Commands.AntitheftSet do
        manufacturer_id: manufacturer_id,
        antitheft_hint: antitheft_hint,
        locking_entity_id: locking_entity_id
+     ]}
+  end
+
+  # v2
+  def decode_params(
+        <<state_bit::size(1), magic_code_length::size(7),
+          magic_code::binary-size(magic_code_length), manufacturer_id::size(16),
+          antitheft_hint_length, antitheft_hint::binary-size(antitheft_hint_length)>>
+      ) do
+    state = Antitheft.state_from_bit(state_bit)
+
+    {:ok,
+     [
+       state: state,
+       magic_code: magic_code,
+       manufacturer_id: manufacturer_id,
+       antitheft_hint: antitheft_hint
      ]}
   end
 end
--- test/grizzly/zwave/commands/antitheft_set_test.exs
+++ test/grizzly/zwave/commands/antitheft_set_test.exs
@@ -3,54 +3,108 @@ defmodule Grizzly.ZWave.Commands.AntitheftSetTest do
 
   alias Grizzly.ZWave.Commands.AntitheftSet
 
-  test "creates the command and validates params" do
-    params = [
-      state: :locked,
-      magic_code: "hocuspocus",
-      manufacturer_id: 126,
-      antitheft_hint: "rabbit",
-      locking_entity_id: 341
-    ]
+  describe "creates the command and validates params" do
+    test "v2" do
+      params = [
+        state: :locked,
+        magic_code: "hocuspocus",
+        manufacturer_id: 126,
+        antitheft_hint: "rabbit"
+      ]
 
-    {:ok, _command} = AntitheftSet.new(params)
+      {:ok, _command} = AntitheftSet.new(params)
+    end
+
+    test "v3" do
+      params = [
+        state: :locked,
+        magic_code: "hocuspocus",
+        manufacturer_id: 126,
+        antitheft_hint: "rabbit",
+        locking_entity_id: 341
+      ]
+
+      {:ok, _command} = AntitheftSet.new(params)
+    end
   end
 
-  test "encodes params correctly" do
-    magic_code = "hocuspocus"
-    hint = "rabbit"
+  describe "encodes params correctly" do
+    test "v2" do
+      magic_code = "hocuspocus"
+      hint = "rabbit"
 
-    params = [
-      state: :locked,
-      magic_code: magic_code,
-      manufacturer_id: 126,
-      antitheft_hint: hint,
-      locking_entity_id: 341
-    ]
+      params = [
+        state: :locked,
+        magic_code: magic_code,
+        manufacturer_id: 126,
+        antitheft_hint: hint
+      ]
 
-    {:ok, command} = AntitheftSet.new(params)
+      {:ok, command} = AntitheftSet.new(params)
 
-    expected_binary =
-      <<0x01::size(1), 10::size(7)>> <>
-        magic_code <>
-        <<126::size(16), 6>> <> hint <> <<341::size(16)>>
+      expected_binary =
+        <<0x01::size(1), 10::size(7)>> <>
+          magic_code <>
+          <<126::size(16), 6>> <> hint
 
-    assert expected_binary == AntitheftSet.encode_params(command)
+      assert expected_binary == AntitheftSet.encode_params(command)
+    end
+
+    test "v3" do
+      magic_code = "hocuspocus"
+      hint = "rabbit"
+
+      params = [
+        state: :locked,
+        magic_code: magic_code,
+        manufacturer_id: 126,
+        antitheft_hint: hint,
+        locking_entity_id: 341
+      ]
+
+      {:ok, command} = AntitheftSet.new(params)
+
+      expected_binary =
+        <<0x01::size(1), 10::size(7)>> <>
+          magic_code <>
+          <<126::size(16), 6>> <> hint <> <<341::size(16)>>
+
+      assert expected_binary == AntitheftSet.encode_params(command)
+    end
   end
 
-  test "decodes params correctly" do
-    magic_code = "hocuspocus"
-    hint = "rabbit"
+  describe "decodes params correctly" do
+    test "v2" do
+      magic_code = "hocuspocus"
+      hint = "rabbit"
 
-    params_binary =
-      <<0x01::size(1), 10::size(7)>> <>
-        magic_code <>
-        <<126::size(16), 6>> <> hint <> <<341::size(16)>>
+      params_binary =
+        <<0x01::size(1), 10::size(7)>> <>
+          magic_code <>
+          <<126::size(16), 6>> <> hint
 
-    {:ok, params} = AntitheftSet.decode_params(params_binary)
-    assert Keyword.get(params, :state) == :locked
-    assert Keyword.get(params, :magic_code) == magic_code
-    assert Keyword.get(params, :manufacturer_id) == 126
-    assert Keyword.get(params, :antitheft_hint) == hint
-    assert Keyword.get(params, :locking_entity_id) == 341
+      {:ok, params} = AntitheftSet.decode_params(params_binary)
+      assert Keyword.get(params, :state) == :locked
+      assert Keyword.get(params, :magic_code) == magic_code
+      assert Keyword.get(params, :manufacturer_id) == 126
+      assert Keyword.get(params, :antitheft_hint) == hint
+    end
+
+    test "v3" do
+      magic_code = "hocuspocus"
+      hint = "rabbit"
+
+      params_binary =
+        <<0x01::size(1), 10::size(7)>> <>
+          magic_code <>
+          <<126::size(16), 6>> <> hint <> <<341::size(16)>>
+
+      {:ok, params} = AntitheftSet.decode_params(params_binary)
+      assert Keyword.get(params, :state) == :locked
+      assert Keyword.get(params, :magic_code) == magic_code
+      assert Keyword.get(params, :manufacturer_id) == 126
+      assert Keyword.get(params, :antitheft_hint) == hint
+      assert Keyword.get(params, :locking_entity_id) == 341
+    end
   end
 end
