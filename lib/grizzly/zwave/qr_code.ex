8185b7cfd977fb2d12c19608609d114a90fc4e08
Matt Ludwigs <mattludwigs@users.noreply.github.com>
2021-02-09 11:17:41 -0700

    Add QR code generation support
    This adds support for generating QR Codes for smart start and S2
    devices.

---
+++ lib/grizzly/zwave/qr_code.ex
@@ -0,0 +1,141 @@
+defmodule Grizzly.ZWave.QRCode do
+  @moduledoc """
+  Z-Wave QR code
+  This module handles Z-Wave QR codes that follow Silicon Labs
+  Software Design Specification SDS13937 and SDS13944.
+  """
+
+  alias Grizzly.ZWave
+  alias Grizzly.ZWave.SmartStart.MetaExtension.UUID16
+
+  @typedoc "QR Code version (S2-only or Smart Start-enabled)"
+  @type version() :: :s2 | :smart_start
+
+  @typedoc """
+  Device information
+  * `:zwave_device_type` - either {generic_device_type, specific_device_type} or a number
+  """
+  @type t() :: %__MODULE__{
+          version: version(),
+          requested_keys: [ZWave.Security.key()],
+          dsk: ZWave.DSK.t(),
+          zwave_device_type: {atom(), atom()} | 0..65535,
+          zwave_installer_icon: ZWave.IconType.name() | ZWave.IconType.value(),
+          manufacturer_id: 0..65535,
+          product_type: 0..65535,
+          product_id: 0..65535,
+          application_version: 0..65535,
+          uuid16: nil | UUID16.t()
+        }
+
+  defstruct version: :smart_start,
+            requested_keys: [],
+            dsk: ZWave.DSK.zeros(),
+            zwave_device_type: 0,
+            zwave_installer_icon: 0,
+            manufacturer_id: 0,
+            product_type: 0,
+            product_id: 0,
+            application_version: 0,
+            uuid16: nil
+
+  @lead_in "90"
+
+  @doc """
+  Encode device information into Z-Wave QR Code format
+  The output of this is either a 90 byte or 136 byte string that should be put
+  into a QR Code. Z-Wave specifies that the 90-byte code (no UUID16) is made
+  into a 29x29 pixel QR Code. The 136-byte* code (w/ UUID16) should be put into
+  a 33x33 pixel QR Code.
+  QR Codes should be encoded as type "text" with error correction "L".
+  NOTE: SDS13937 has a mistake with the code length. It says 134 bytes, but the
+  UUID16 encode has 2 extra bytes for presentation, so it should be 136.
+  """
+  @spec encode!(t()) :: iolist()
+  def encode!(info) do
+    payload = [
+      encode_requested_keys(info.requested_keys),
+      encode_dsk(info),
+      encode_qr_product_type(info),
+      encode_qr_product_id(info),
+      encode_uuid16(info.uuid16)
+    ]
+
+    [@lead_in, encode_version(info.version), checksum(payload), payload]
+  end
+
+  defp checksum(payload) do
+    <<two_bytes::16, _rest::144>> = :crypto.hash(:sha, payload)
+    int_to_string(two_bytes, 5)
+  end
+
+  defp encode_version(:s2), do: "00"
+  defp encode_version(:smart_start), do: "01"
+
+  defp encode_requested_keys(requested_keys) do
+    ZWave.Security.keys_to_byte(requested_keys)
+    |> int_to_string(3)
+  end
+
+  defp encode_dsk(info), do: ZWave.DSK.to_string(info.dsk, delimiter: "")
+
+  defp encode_qr_product_type(info) do
+    # QR Product type = TLV type 00, length 10
+    [
+      "0010",
+      encode_device_type(info.zwave_device_type),
+      encode_icon_type(info.zwave_installer_icon)
+    ]
+  end
+
+  defp encode_device_type({generic, specific}) do
+    device_type =
+      ZWave.DeviceClasses.generic_device_class_to_byte(generic) * 256 +
+        ZWave.DeviceClasses.specific_device_class_to_byte(generic, specific)
+
+    encode_device_type(device_type)
+  end
+
+  defp encode_device_type(device_type) when is_integer(device_type) do
+    int_to_string(device_type, 5)
+  end
+
+  defp encode_icon_type(type) when is_atom(type) do
+    {:ok, value} = ZWave.IconType.to_value(type)
+    encode_icon_type(value)
+  end
+
+  defp encode_icon_type(type), do: int_to_string(type, 5)
+
+  defp encode_qr_product_id(info) do
+    # QR Product ID = TLV type 02, length 20
+    [
+      "0220",
+      int_to_string(info.manufacturer_id, 5),
+      int_to_string(info.product_type, 5),
+      int_to_string(info.product_id, 5),
+      int_to_string(info.application_version, 5)
+    ]
+  end
+
+  defp encode_uuid16(nil), do: []
+
+  defp encode_uuid16(uuid16) do
+    {:ok, value} = UUID16.to_binary(uuid16)
+    <<0x06, 0x11, presentation, uuid::binary()>> = value
+
+    decimalized_uuid =
+      uuid
+      |> ZWave.DSK.new()
+      |> ZWave.DSK.to_string(delimiter: "")
+
+    # UUID16 = TLV type 06, length 42
+    ["0642", int_to_string(presentation, 2), decimalized_uuid]
+  end
+
+  defp int_to_string(value, num_digits) when value >= 0 do
+    value
+    |> Integer.to_string(10)
+    |> String.pad_leading(num_digits, "0")
+  end
+end
---
+++ test/grizzly/zwave/qr_code_test.exs
@@ -0,0 +1,115 @@
+defmodule Grizzly.ZWave.QRCodeTest do
+  use ExUnit.Case
+
+  alias Grizzly.ZWave.{DSK, QRCode}
+  alias Grizzly.ZWave.SmartStart.MetaExtension.UUID16
+
+  defp encode!(info) do
+    QRCode.encode!(info) |> IO.iodata_to_binary()
+  end
+
+  test "basic Smart Start code" do
+    {:ok, dsk} = DSK.parse("51525-35455-41424-34445-31323-33435-21222-32425")
+    # This is the ACME Light Dimmer example
+    info = %QRCode{
+      version: :smart_start,
+      dsk: dsk,
+      requested_keys: [:s2_authenticated, :s2_unauthenticated],
+      zwave_device_type: {:switch_multilevel, :power_switch_multilevel},
+      zwave_installer_icon: :specific_light_dimmer_plugin,
+      manufacturer_id: 0xFFF0,
+      product_type: 0x64,
+      product_id: 3,
+      application_version: 0x0242
+    }
+
+    encoded = encode!(info)
+    assert byte_size(encoded) == 90
+
+    assert encoded ==
+             "900132782003515253545541424344453132333435212223242500100435301537022065520001000000300578"
+  end
+
+  test "code with UUID" do
+    # This is the Oscorp Door Lock w. UUID example
+    {:ok, uuid16} = UUID16.new("52E67EA9A1D0868D2B717AB77A5B829B", :hex)
+    {:ok, dsk} = DSK.parse("51525 35455 41424 34445 31323 33435 21222 32425")
+
+    info = %QRCode{
+      version: :smart_start,
+      requested_keys: [:s2_access_control, :s2_authenticated, :s2_unauthenticated],
+      dsk: dsk,
+      zwave_device_type: {:entry_control, :secure_keypad_door_lock},
+      zwave_installer_icon: :generic_door_lock_keypad,
+      manufacturer_id: 0xFFF1,
+      product_type: 0x3E8,
+      product_id: 0x11,
+      application_version: 0x0120,
+      uuid16: uuid16
+    }
+
+    encoded = encode!(info)
+    assert byte_size(encoded) == 136
+
+    assert encoded ==
+             "9001346230075152535455414243444531323334352122232425001016387007680220655210100000017002880642002122232425414243444511121314153132333435"
+  end
+
+  test "S2-only qr code" do
+    {:ok, dsk} = DSK.parse("51525-35455-41424-34445-31323-33435-21222-32425")
+    # This is the ACME Light Dimmer (S2-only - not SmartStart) example
+    info = %QRCode{
+      version: :s2,
+      requested_keys: [:s2_authenticated, :s2_unauthenticated],
+      dsk: dsk,
+      zwave_device_type: {:switch_multilevel, :power_switch_multilevel},
+      zwave_installer_icon: :specific_light_dimmer_plugin,
+      manufacturer_id: 0xFFF0,
+      product_type: 0x64,
+      product_id: 3,
+      application_version: 0x0242
+    }
+
+    assert encode!(info) ==
+             "900032782003515253545541424344453132333435212223242500100435301537022065520001000000300578"
+  end
+
+  defp zwave_decimalize(input) do
+    for <<two_bytes::16 <- input>> do
+      Integer.to_string(two_bytes, 10) |> String.pad_leading(5, "0") |> IO.inspect()
+    end
+    |> IO.iodata_to_binary()
+  end
+
+  test "creating code for hypothetical hub" do
+    {:ok, dsk} = DSK.parse("63239-37621-03012-10660-02008-55918-62746-33260")
+    serial = "ABC123456789"
+    padded_serial = String.pad_trailing(serial, 16)
+    {:ok, uuid16} = UUID16.new("sn:" <> padded_serial, :ascii)
+
+    info = %QRCode{
+      version: :smart_start,
+      dsk: dsk,
+      requested_keys: [:s2_authenticated, :s2_unauthenticated],
+      zwave_device_type: {:static_controller, :gateway},
+      zwave_installer_icon: :gateway,
+      manufacturer_id: 0x0390,
+      product_type: 1,
+      product_id: 1,
+      application_version: 1,
+      uuid16: uuid16
+    }
+
+    encoded = encode!(info)
+
+    assert byte_size(encoded) == 136
+
+    # Verify that the serial number was properly padded and encoded by
+    # re-implementing the algorithm here.
+    encoded_serial = zwave_decimalize(padded_serial)
+    assert String.ends_with?(encoded, encoded_serial)
+
+    assert encode!(info) ==
+             "9001135290036323937621030121066002008559186274633260001000519012800220009120000100001000010642031670617201128511336513879143930822408224"
+  end
+end
