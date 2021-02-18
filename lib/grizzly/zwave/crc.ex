72a102883aa366d45a624220c831eb6ba991a87c
Frank Hunleth <fhunleth@troodon-software.com>
2021-02-17 21:13:53 -0500

    Use cerlc to compute CRCs
    cerlc is a new small and pure Erlang CRC library. This lets us remove
    the CRC code here and benefit from any performance improvements made in
    cerlc.

--- lib/grizzly/zwave/crc.ex
+++ lib/grizzly/zwave/crc.ex
@@ -6,31 +6,14 @@ defmodule Grizzly.ZWave.CRC do
   exposes CRC functions to be able to run those checks.
   """
 
-  use Bitwise
-
-  @table Grizzly.ZWave.CRC.Table.ccitt_table(0x1021)
-
+  @crc16_aug_ccitt :cerlc.init(:crc16_aug_ccitt)
   @type uint16() :: 0..65535
 
   @doc """
   CRC-16/AUG-CCITT
   """
   @spec crc16_aug_ccitt(binary() | [byte()]) :: uint16()
-  def crc16_aug_ccitt(data) when is_binary(data) do
-    data
-    |> :binary.bin_to_list()
-    |> crc16_aug_ccitt()
+  def crc16_aug_ccitt(data) do
+    :cerlc.calc_crc(data, @crc16_aug_ccitt)
   end
-
-  def crc16_aug_ccitt(data) when is_list(data) do
-    crc_ccitt(data, 0x1D0F)
-  end
-
-  defp crc_ccitt([x | rem], crc) do
-    index = (crc >>> 8) ^^^ x
-    crc = (crc <<< 8) ^^^ elem(@table, index) &&& 0xFFFF
-    crc_ccitt(rem, crc)
-  end
-
-  defp crc_ccitt([], crc), do: crc
 end
