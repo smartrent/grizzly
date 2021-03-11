b5b3cd7579b81ff5971ec0151f5be40285aed5af
Frank Hunleth <fhunleth@troodon-software.com>
2021-03-11 14:21:02 -0500

    Ensure that the DSK binary is 128 bits; add test
    DSK.new/1 was untested. This fixes that.
    
    The type spec for DSK.t() also requires that the binary be 128 bits
    internally. This is nice since it means that DSK functions don't have to
    worry about empty or short binaries. It, however, was not implemented
    that way and binaries could be any even size between 0 and 16 bytes.

--- test/grizzly/zwave/dsk_test.exs
+++ test/grizzly/zwave/dsk_test.exs
@@ -7,9 +7,30 @@ defmodule Grizzly.ZWave.DSKTest do
   @dsk_string "33654-49908-42539-00289-58381-21884-63570-22247"
   @dsk_binary <<33654::16, 49908::16, 42539::16, 00289::16, 58381::16, 21884::16, 63570::16,
                 22247::16>>
-  @dsk_struct %DSK{raw: @dsk_binary}
+  @dsk_struct DSK.new(@dsk_binary)
 
   test "new/1" do
+    # Normal case
+    assert %DSK{raw: @dsk_binary} == DSK.new(@dsk_binary)
+
+    # 5-digit partial DSK case when handling PINs
+    assert %DSK{raw: <<12345::16, 0::112>>} == DSK.new(<<12345::16>>)
+
+    # Empty
+    assert %DSK{raw: <<0::128>>} == DSK.new(<<>>)
+
+    # Odd lengths in bytes
+    assert_raise FunctionClauseError, fn -> DSK.new(<<123::8>>) end
+    assert_raise FunctionClauseError, fn -> DSK.new(<<123::24>>) end
+    assert_raise FunctionClauseError, fn -> DSK.new(<<123::40>>) end
+    assert_raise FunctionClauseError, fn -> DSK.new(<<123::56>>) end
+    assert_raise FunctionClauseError, fn -> DSK.new(<<123::72>>) end
+    assert_raise FunctionClauseError, fn -> DSK.new(<<123::88>>) end
+    assert_raise FunctionClauseError, fn -> DSK.new(<<123::104>>) end
+    assert_raise FunctionClauseError, fn -> DSK.new(<<123::120>>) end
+
+    # Too long
+    assert_raise FunctionClauseError, fn -> DSK.new(<<123::144>>) end
   end
 
   test "parse/1" do
@@ -17,8 +38,8 @@ defmodule Grizzly.ZWave.DSKTest do
     assert {:ok, @dsk_struct} == DSK.parse(String.replace(@dsk_string, "-", " "))
     assert {:ok, @dsk_struct} == DSK.parse(String.replace(@dsk_string, "-", ""))
 
-    assert {:ok, %DSK{raw: <<12345::16>>}} == DSK.parse("12345")
-    assert {:ok, %DSK{raw: <<0::16>>}} == DSK.parse("00000")
+    assert {:ok, DSK.new(<<12345::16, 0::112>>)} == DSK.parse("12345")
+    assert {:ok, DSK.new(<<0::128>>)} == DSK.parse("00000")
 
     assert {:error, :invalid_dsk} == DSK.parse(@dsk_string <> "12345")
     assert {:error, :invalid_dsk} == DSK.parse("")
