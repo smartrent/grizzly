1e286173bfaed3ec4211b348b847bf5b5c88df33
Matt Ludwigs <mattludwigs@users.noreply.github.com>
2021-04-01 14:00:45 -0700

    Support long range NetworkManagementInclusion command class
    Adds support the updates needed for our existing command classes to
    parse 16 bit node ids.

--- test/grizzly/zwave/commands/failed_node_remove_status_test.exs
+++ test/grizzly/zwave/commands/failed_node_remove_status_test.exs
@@ -15,11 +15,21 @@ defmodule Grizzly.ZWave.Commands.FailedNodeRemoveStatusTest do
     assert expected_binary == FailedNodeRemoveStatus.encode_params(command)
   end
 
-  test "decodes params correctly" do
-    params_binary = <<0x02, 0x00, 0x04>>
-    {:ok, params} = FailedNodeRemoveStatus.decode_params(params_binary)
-    assert Keyword.get(params, :seq_number) == 2
-    assert Keyword.get(params, :node_id) == 4
-    assert Keyword.get(params, :status) == :failed_node_not_found
+  describe "decodes params correctly" do
+    test "NetworkWorkManagementInclusion < v3" do
+      params_binary = <<0x02, 0x00, 0x04>>
+      {:ok, params} = FailedNodeRemoveStatus.decode_params(params_binary)
+      assert Keyword.get(params, :seq_number) == 2
+      assert Keyword.get(params, :node_id) == 4
+      assert Keyword.get(params, :status) == :failed_node_not_found
+    end
+
+    test "NetworkWorkManagementInclusion < v4 (long range)" do
+      params_binary = <<0x02, 0x00, 0x01, 0x00>>
+      {:ok, params} = FailedNodeRemoveStatus.decode_params(params_binary)
+      assert Keyword.get(params, :seq_number) == 2
+      assert Keyword.get(params, :node_id) == 256
+      assert Keyword.get(params, :status) == :failed_node_not_found
+    end
   end
 end
--- test/grizzly/zwave/commands/failed_node_remove_test.exs
+++ test/grizzly/zwave/commands/failed_node_remove_test.exs
@@ -15,10 +15,19 @@ defmodule Grizzly.ZWave.Commands.FailedNodeRemoveTest do
     assert expected_binary == FailedNodeRemove.encode_params(command)
   end
 
-  test "decodes params correctly" do
-    params_binary = <<0x02, 0x04>>
-    {:ok, params} = FailedNodeRemove.decode_params(params_binary)
-    assert Keyword.get(params, :seq_number) == 2
-    assert Keyword.get(params, :node_id) == 4
+  describe "decodes params correctly" do
+    test "NetworkManagementInclusion < v3" do
+      params_binary = <<0x02, 0x04>>
+      {:ok, params} = FailedNodeRemove.decode_params(params_binary)
+      assert Keyword.get(params, :seq_number) == 2
+      assert Keyword.get(params, :node_id) == 4
+    end
+
+    test "NetworkManagementInclusion < v4 (long range)" do
+      params_binary = <<0x02, 0x01, 0x00>>
+      {:ok, params} = FailedNodeRemove.decode_params(params_binary)
+      assert Keyword.get(params, :seq_number) == 2
+      assert Keyword.get(params, :node_id) == 256
+    end
   end
 end
