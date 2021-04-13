721826953b1917327d7dcd69aff8cf5e6e5ce564
Jean-Francois Cloutier <jean.f.cloutier@gmail.com>
2021-04-13 10:54:38 -0400

    Support version 2 of UsersNumberReport

--- test/grizzly/zwave/commands/user_code_users_number_report_test.exs
+++ test/grizzly/zwave/commands/user_code_users_number_report_test.exs
@@ -3,24 +3,54 @@ defmodule Grizzly.ZWave.Commands.UserCodeUsersNumberReportTest do
 
   alias Grizzly.ZWave.Commands.UserCodeUsersNumberReport
 
-  test "creates the command and validates params" do
-    params = [supported_users: 24]
-    {:ok, _command} = UserCodeUsersNumberReport.new(params)
+  # V1
+
+  describe "creates the command and validates params" do
+    test "v1" do
+      params = [supported_users: 24]
+      {:ok, _command} = UserCodeUsersNumberReport.new(params)
+    end
+
+    test "v2" do
+      params = [supported_users: 24, extended_supported_users: 24]
+      {:ok, _command} = UserCodeUsersNumberReport.new(params)
+    end
   end
 
-  test "encodes params correctly" do
-    params = [supported_users: 24]
-    {:ok, command} = UserCodeUsersNumberReport.new(params)
+  describe "encodes params correctly" do
+    test "v1" do
+      params = [supported_users: 24]
+      {:ok, command} = UserCodeUsersNumberReport.new(params)
 
-    expected_binary = <<0x18>>
+      expected_binary = <<0x18>>
 
-    assert expected_binary == UserCodeUsersNumberReport.encode_params(command)
+      assert expected_binary == UserCodeUsersNumberReport.encode_params(command)
+    end
+
+    test "v2" do
+      params = [supported_users: 24, extended_supported_users: 24]
+      {:ok, command} = UserCodeUsersNumberReport.new(params)
+
+      expected_binary = <<0x18, 0x00, 0x18>>
+
+      assert expected_binary == UserCodeUsersNumberReport.encode_params(command)
+    end
   end
 
-  test "decodes params correctly" do
-    binary_params = <<0x18>>
+  describe "decodes params correctly" do
+    test "v1" do
+      binary_params = <<0x18>>
 
-    {:ok, params} = UserCodeUsersNumberReport.decode_params(binary_params)
-    assert Keyword.get(params, :supported_users) == 24
+      {:ok, params} = UserCodeUsersNumberReport.decode_params(binary_params)
+      assert Keyword.get(params, :supported_users) == 24
+    end
+
+    test "v2" do
+      binary_params = <<0x18, 0x00, 0x18>>
+
+      {:ok, params} = UserCodeUsersNumberReport.decode_params(binary_params)
+      assert Keyword.get(params, :supported_users) == 24
+      assert Keyword.get(params, :extended_supported_users) == 24
+    end
   end
 end
