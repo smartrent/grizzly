124eda38b3bf53575323dc68f3198e85f5ebd23a
Matt Ludwigs <mattludwigs@users.noreply.github.com>
2021-09-27 09:47:23 -0700

    Add `node_add_status_to_byte/1` function

--- lib/grizzly/zwave/command_classes/network_management_inclusion.ex
+++ lib/grizzly/zwave/command_classes/network_management_inclusion.ex
@@ -9,7 +9,14 @@ defmodule Grizzly.ZWave.CommandClasses.NetworkManagementInclusion do
   @behaviour Grizzly.ZWave.CommandClass
 
   @typedoc """
+  The status of the inclusion process
 
+  * `:done` - the inclusion process is done without error
+  * `:failed` - the inclusion process is done with failure, the device is not
+    included
+  * `:security_failed` - the inclusion process is done, the device is included
+    but their was an error during the security negotiations. Device \
+    functionality will be degraded.
   """
   @type node_add_status() :: :done | :failed | :security_failed
 
@@ -26,4 +33,12 @@ defmodule Grizzly.ZWave.CommandClasses.NetworkManagementInclusion do
   def parse_node_add_status(0x06), do: :done
   def parse_node_add_status(0x07), do: :failed
   def parse_node_add_status(0x09), do: :security_failed
+
+  @doc """
+  Encode a `node_add_status()` to a byte
+  """
+  @spec node_add_status_to_byte(node_add_status()) :: 0x06 | 0x07 | 0x09
+  def node_add_status_to_byte(:done), do: 0x06
+  def node_add_status_to_byte(:failed), do: 0x07
+  def node_add_status_to_byte(:security_failed), do: 0x09
 end
