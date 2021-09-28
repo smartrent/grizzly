ad7b5df67ab40388f41b68c2cf7bcb46ed14fabb
Matt Ludwigs <mattludwigs@users.noreply.github.com>
2021-09-28 14:39:40 -0700

    Support version 4 FailedNodeRemoveStatus

--- test/grizzly/zwave/commands/failed_node_remove_status_test.exs
+++ test/grizzly/zwave/commands/failed_node_remove_status_test.exs
@@ -8,28 +8,60 @@ defmodule Grizzly.ZWave.Commands.FailedNodeRemoveStatusTest do
     {:ok, _command} = FailedNodeRemoveStatus.new(params)
   end
 
-  test "encodes params correctly" do
-    params = [seq_number: 2, node_id: 4, status: :done]
-    {:ok, command} = FailedNodeRemoveStatus.new(params)
-    expected_binary = <<0x02, 0x01, 0x04>>
-    assert expected_binary == FailedNodeRemoveStatus.encode_params(command)
-  end
+  describe "encoding" do
+    test "version 1-3 - only 8 bit node id" do
+      expected_bin = <<0x01, 0x01, 0x04>>
+      {:ok, command} = FailedNodeRemoveStatus.new(seq_number: 0x01, status: :done, node_id: 0x04)
 
-  describe "decodes params correctly" do
-    test "NetworkWorkManagementInclusion < v3" do
-      params_binary = <<0x02, 0x00, 0x04>>
-      {:ok, params} = FailedNodeRemoveStatus.decode_params(params_binary)
-      assert Keyword.get(params, :seq_number) == 2
-      assert Keyword.get(params, :node_id) == 4
-      assert Keyword.get(params, :status) == :failed_node_not_found
+      assert FailedNodeRemoveStatus.encode_params(command, command_class_version: 3) ==
+               expected_bin
     end
 
-    test "NetworkWorkManagementInclusion < v4 (long range)" do
-      params_binary = <<0x02, 0x00, 0x01, 0x00>>
-      {:ok, params} = FailedNodeRemoveStatus.decode_params(params_binary)
-      assert Keyword.get(params, :seq_number) == 2
-      assert Keyword.get(params, :node_id) == 256
-      assert Keyword.get(params, :status) == :failed_node_not_found
+    test "version 4 - with 8 bit node ids" do
+      expected_bin = <<0x01, 0x01, 0x04, 0x00, 0x00>>
+      {:ok, command} = FailedNodeRemoveStatus.new(seq_number: 0x01, status: :done, node_id: 0x04)
+
+      assert FailedNodeRemoveStatus.encode_params(command) ==
+               expected_bin
+    end
+
+    test "version 4 - with 16 bit node ids" do
+      expected_bin = <<0x01, 0x01, 0xFF, 0x15, 0xAC>>
+
+      {:ok, command} =
+        FailedNodeRemoveStatus.new(seq_number: 0x01, status: :done, node_id: 0x15AC)
+
+      assert FailedNodeRemoveStatus.encode_params(command) ==
+               expected_bin
+    end
+  end
+
+  describe "decoding" do
+    test "version 1-3 - only 8 bit node id" do
+      expected_params = [seq_number: 0x02, node_id: 0x04, status: :failed_node_not_found]
+      {:ok, params} = FailedNodeRemoveStatus.decode_params(<<0x02, 0x00, 0x04>>)
+
+      assert_params(expected_params, params)
+    end
+
+    test "version 4 - with 8 bit node ids" do
+      expected_params = [seq_number: 0x02, node_id: 0x04, status: :failed_node_not_found]
+      {:ok, params} = FailedNodeRemoveStatus.decode_params(<<0x02, 0x00, 0x04, 0x00, 0x00>>)
+
+      assert_params(expected_params, params)
+    end
+
+    test "version 4 - with 16 bit node ids" do
+      expected_params = [seq_number: 0x02, node_id: 0x043B, status: :failed_node_not_found]
+      {:ok, params} = FailedNodeRemoveStatus.decode_params(<<0x02, 0x00, 0xFF, 0x04, 0x3B>>)
+
+      assert_params(expected_params, params)
+    end
+  end
+
+  defp assert_params(expected_params, params) do
+    for {param, value} <- expected_params do
+      assert params[param] == value
     end
   end
 end
