a54b12470d9a70fd3338cc62adf1b280bed1de29
Matt Ludwigs <mattludwigs@users.noreply.github.com>
2021-09-29 14:01:48 -0700

    Add ZWaveLongRangeChannelGet command

---
+++ lib/grizzly/zwave/commands/zwave_long_range_channel_get.ex
@@ -0,0 +1,30 @@
+defmodule Grizzly.ZWave.Commands.ZWaveLongRangeChannelGet do
+  @moduledoc """
+  Command to request the currently configured Z-Wave Long Range Channel
+
+  Params: -none-
+  """
+
+  @behaviour Grizzly.ZWave.Command
+
+  alias Grizzly.ZWave.Command
+  alias Grizzly.ZWave.CommandClasses.NetworkManagementInstallationMaintenance
+
+  @impl Grizzly.ZWave.Command
+  def new(_opts \\ []) do
+    command = %Command{
+      name: :zwave_long_range_channel_get,
+      command_byte: 0x0D,
+      command_class: NetworkManagementInstallationMaintenance,
+      impl: __MODULE__
+    }
+
+    {:ok, command}
+  end
+
+  @impl Grizzly.ZWave.Command
+  def decode_params(_), do: {:ok, []}
+
+  @impl Grizzly.ZWave.Command
+  def encode_params(_), do: <<>>
+end
