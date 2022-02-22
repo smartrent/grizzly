18303392233d76c8ab21004b4c4a9930e15f63bd
Jean-Francois Cloutier <jean.f.cloutier@gmail.com>
2022-02-22 13:52:21 -0500

    association group command list report fix
    Ignores bytes added by some devices beyond the advertized commands count

--- test/grizzly/zwave/commands/association_group_command_list_report_test.exs
+++ test/grizzly/zwave/commands/association_group_command_list_report_test.exs
@@ -23,4 +23,13 @@ defmodule Grizzly.ZWave.Commands.AssociationGroupCommandListReportTest do
     assert Enum.count(commands) == 3
     assert Enum.all?([:basic_set, :basic_get, :battery_get], &(&1 in commands)) == true
   end
+
+  test "decodes params correctly in spite of extra byte" do
+    binary_params = <<0x02, 0x06, 0x20, 0x01, 0x20, 0x02, 0x80, 0x02, 0x00>>
+    {:ok, params} = AssociationGroupCommandListReport.decode_params(binary_params)
+    assert Keyword.get(params, :group_id) == 2
+    commands = Keyword.get(params, :commands)
+    assert Enum.count(commands) == 3
+    assert Enum.all?([:basic_set, :basic_get, :battery_get], &(&1 in commands)) == true
+  end
 end
