ee36c857c1de362a50ab1a03d7ebbfd87cd0058e
Matt Ludwigs <mattludwigs@users.noreply.github.com>
2022-02-28 14:49:17 -0700

    Ignore new dialyzer warnings

--- dialyzer_ignore_warnings.exs
+++ dialyzer_ignore_warnings.exs
@@ -1,5 +1,9 @@
 [
   # {"The call _@2:'command'() requires that _@2 is of type atom() not binary()"}
   {"lib/grizzly/connections/async_connection.ex"},
-  {"lib/grizzly/connections/sync_connection.ex"}
+  {"lib/grizzly/connections/sync_connection.ex"},
+  # ignore warnings in UUID16 as they are from a bug in Elixir typespecs that
+  # has been fixed but not release: https://github.com/elixir-lang/elixir/pull/11449
+  # Once a new release (> v1.13.3) is ready we should remove this ignore.
+  {"lib/grizzly/zwave/smart_start/meta_extension/uuid16.ex"}
 ]
