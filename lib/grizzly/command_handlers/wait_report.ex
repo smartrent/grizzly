1974b3f195ee2c5a48294de9620642d53c69dc9e
Jean-Francois Cloutier <jean.f.cloutier@gmail.com>
2023-03-06 14:52:36 -0500

    Wait on :application_busy commands
    SRH-644

--- lib/grizzly/command_handlers/wait_report.ex
+++ lib/grizzly/command_handlers/wait_report.ex
@@ -7,6 +7,8 @@ defmodule Grizzly.CommandHandlers.WaitReport do
 
   alias Grizzly.ZWave.Command
 
+  require Logger
+
   @type state :: %{complete_report: atom()}
 
   @type opt :: {:complete_report, atom()}
@@ -26,7 +28,25 @@ defmodule Grizzly.CommandHandlers.WaitReport do
     if state.complete_report == :any or command.name == state.complete_report do
       {:complete, command}
     else
+      if command.name == :application_busy,
+        do: wait_application_busy(command)
+
       {:continue, state}
     end
+  end
+
+  defp wait_application_busy(command) do
+    status = Keyword.get(command.params, :status)
+    wait_time_secs = Keyword.get(command.params, :wait_time, 1)
+
+    delay =
+      case status do
+        :try_again_later -> 500
+        :try_again_after_wait -> wait_time_secs * 1_000
+        :request_queued -> 0
+      end
+
+    Logger.warn("[Grizzly] Application busy - waiting #{delay} msecs")
+    :timer.sleep(delay)
   end
 end
