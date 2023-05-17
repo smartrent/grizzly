d9c5520394883565e374478a92652d00bfa0e31a
Ben Youngblood <bx.youngblood@gmail.com>
2023-05-17 12:15:07 -0700

    Support signed values in Thermostat Setpoint Set
    The Thermostat Setpoint Set CC uses signed values in its binary
    encoding, but we were encoding and decoding values as unsigned.
    
    On top of being incorrect, the calculation for the number of bytes
    needed to encode the value would raise an error for any negative number
    (because log-n is undefined for values <= 0).
    
    Co-Authored-By: Mike McCall <mike.mccall@smartrent.com>

--- lib/grizzly/zwave/commands/thermostat_setpoint_set.ex
+++ lib/grizzly/zwave/commands/thermostat_setpoint_set.ex
@@ -41,16 +41,16 @@ defmodule Grizzly.ZWave.Commands.ThermostatSetpointSet do
     value = Command.param!(command, :value)
     precision = precision(value)
     int_value = round(value * :math.pow(10, precision))
-    byte_size = ceil(:math.log2(int_value) / 8)
+    byte_size = __bytes_needed__(int_value)
 
     <<0x00::size(4), type_byte::size(4), precision::size(3), scale_byte::size(2),
-      byte_size::size(3), int_value::size(byte_size)-unit(8)>>
+      byte_size::size(3), int_value::signed-size(byte_size)-unit(8)>>
   end
 
   @impl Command
   def decode_params(
         <<_::size(4), type_byte::size(4), precision::size(3), scale_byte::size(2),
-          byte_size::size(3), int_value::size(byte_size)-unit(8)>>
+          byte_size::size(3), int_value::signed-size(byte_size)-unit(8)>>
       ) do
     type = ThermostatSetpoint.decode_type(type_byte)
 
@@ -64,10 +64,29 @@ defmodule Grizzly.ZWave.Commands.ThermostatSetpointSet do
     end
   end
 
-  defp precision(value) when is_number(value) do
+  def precision(value) when is_number(value) do
     case String.split("#{value}", ".") do
       [_] -> 0
       [_, dec] -> String.length(dec)
     end
+  end
+
+  def __bits_needed__(0), do: 1
+
+  def __bits_needed__(int_value) do
+    bits = ceil(:math.log2(abs(int_value)) + 1)
+
+    <<msb::1, _rest::size(bits - 1)>> = <<int_value::signed-size(bits)>>
+
+    if msb == 1 && int_value > 0 do
+      bits + 1
+    else
+      bits
+    end
+  end
+
+  def __bytes_needed__(int_value) do
+    bits = __bits_needed__(int_value)
+    ceil(bits / 8)
   end
 end
