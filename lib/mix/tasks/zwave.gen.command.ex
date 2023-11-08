2e8619d6211c5cd923ec7b62aba6c9d6f726f972
Ben Youngblood <ben.youngblood@smartrent.com>
2023-11-08 12:32:15 -0700

    Improved generator mix commands
    Co-authored-by: Frank Hunleth <frank@smartrent.com>

--- lib/mix/tasks/zwave.gen.command.ex
+++ lib/mix/tasks/zwave.gen.command.ex
@@ -22,25 +22,119 @@ defmodule Mix.Tasks.Zwave.Gen.Command do
   @switches [no_save: :boolean, target_dir: :string, no_test: :boolean]
 
   def run(args) do
-    {opts, [command_name, command_class | params], _} =
+    {opts, args, _} =
       OptionParser.parse(args, switches: @switches)
+
+    command_name = args |> Enum.at(0) |> maybe_prompt("Command:")
+    command_id = args |> Enum.at(1) |> maybe_prompt_for_command_id()
+    command_class = args |> Enum.at(2) |> maybe_prompt("Command Class:")
+
+    params =
+      args
+      |> Enum.drop(3)
+      |> Enum.reverse()
+      |> Enum.map(&{&1, "any()"})
+      |> prompt_for_params(true)
+      |> Enum.reverse()
 
     target_defs = build_target_defs(command_name, opts)
 
-    copy_files(target_defs, command_name, command_class, params, opts)
+    copy_files(target_defs, command_name, command_id, command_class, params, opts)
   end
 
-  defp copy_files(target_defs, command_name, command_class, params, opts) do
+  defp maybe_prompt(value, prompt) do
+    value = value || String.trim(Mix.shell().prompt(prompt))
+
+    if value == "" or not is_binary(value) do
+      maybe_prompt("", prompt)
+    else
+      value
+    end
+  end
+
+  defp maybe_prompt_for_command_id(id \\ nil) do
+    id = id || String.trim(Mix.shell().prompt("Command ID (hex):"))
+
+    case Regex.run(~r/^0?x?([0-9A-Fa-f]{2})$/, id) do
+      [_, id] -> "0x" <> String.upcase(id)
+      _ -> maybe_prompt_for_command_id()
+    end
+  end
+
+  defp print_param_help() do
+    Mix.shell().info("")
+    Mix.shell().info("Enter one of the following commands to add command parameters:")
+    Mix.shell().info("")
+    Mix.shell().info(["* ", :bright, "a <name> [<type>]", :reset, " - add a parameter"])
+    Mix.shell().info(["* ", :bright, "d <name>", :reset, " - delete a parameter by name"])
+    Mix.shell().info(["* ", :bright, "v", :reset, " - show parameter list"])
+    Mix.shell().info(["* ", :bright, "x", :reset, " - done entering parameters"])
+    Mix.shell().info(["* ", :bright, "?", :reset, " - display this message again"])
+  end
+
+  defp print_params(params) do
+    params
+    |> Enum.reverse()
+    |> Enum.each(fn {n, t} ->
+      Mix.shell().info(["* ", :green, :bright, ":#{n}", :reset, " :: ", :yellow, "#{t}", :reset])
+    end)
+  end
+
+  defp prompt_for_params(params, print_help \\ false) do
+    if print_help do
+      print_param_help()
+    end
+
+    Mix.shell().info("")
+
+    cmd =
+      Mix.shell().prompt(">")
+      |> String.trim()
+      |> String.downcase()
+      |> String.split(~r/\s+/)
+
+    case cmd do
+      ["a", name, type] ->
+        type = if(String.ends_with?(type, "()"), do: type, else: type <> "()")
+        params = [{name, type} | params]
+        prompt_for_params(params)
+
+      ["a", name] ->
+        params = [{name, "any()"} | params]
+        prompt_for_params(params)
+
+      ["d", name] ->
+        params = Enum.reject(params, fn {n, _} -> n == name end)
+        prompt_for_params(params)
+
+      ["v"] ->
+        print_params(params)
+        prompt_for_params(params)
+
+      ["?"] ->
+        prompt_for_params(params, true)
+
+      ["x"] ->
+        params
+
+      _ ->
+        prompt_for_params(params)
+    end
+  end
+
+  defp copy_files(target_defs, command_name, command_id, command_class, params, opts) do
     command_module = Module.concat(Grizzly.ZWave.Commands, command_name)
-    command_name = String.to_atom(Macro.underscore(command_name))
+    command_name_snakecase = String.to_atom(Macro.underscore(command_name))
     command_class_module = Module.concat(Grizzly.ZWave.CommandClasses, command_class)
 
     bindings = [
-      params: Enum.map(params, &String.to_atom/1),
-      command_name: command_name,
+      params: params,
+      command_name: command_name_snakecase,
+      command_id: command_id,
       command_module: command_module,
-      command_class: Module.concat([String.to_atom(command_class)]),
-      command_class_module: command_class_module
+      command_module_short: command_name,
+      command_class_module: command_class_module,
+      command_class_module_short: Module.concat([String.to_atom(command_class)])
     ]
 
     Enum.each(target_defs, fn target_def ->
@@ -49,7 +143,14 @@ defmodule Mix.Tasks.Zwave.Gen.Command do
   end
 
   defp to_out({:target, target_file, template_file}, bindings, opts) do
-    evaled = EEx.eval_file(template_file, bindings)
+    evaled = EEx.eval_file(template_file, bindings, trim: true)
+
+    evaled =
+      try do
+        Code.format_string!(evaled)
+      rescue
+        _ -> evaled
+      end
 
     if Keyword.has_key?(opts, :no_save) do
       IO.puts(evaled)
@@ -107,8 +208,8 @@ defmodule Mix.Tasks.Zwave.Gen.Command do
     grizzly_path = Application.app_dir(:grizzly)
     target = get_target(command_name_str, opts)
     test_target = get_test_target(command_name_str, opts)
-    target_template = Path.join(grizzly_path, "/priv/templates/zwave.gen/command.ex")
-    test_template = Path.join(grizzly_path, "/priv/templates/zwave.gen/command_test.exs")
+    target_template = Path.join(grizzly_path, "/priv/templates/zwave.gen/command.eex")
+    test_template = Path.join(grizzly_path, "/priv/templates/zwave.gen/command_test.eex")
 
     [
       {:target, target, target_template},
--- lib/mix/tasks/zwave.gen.command_class.ex
+++ lib/mix/tasks/zwave.gen.command_class.ex
@@ -2,11 +2,11 @@ defmodule Mix.Tasks.Zwave.Gen.CommandClass do
   @moduledoc """
   Generates the scaffolding for a new Z-Wave Command
 
-    mix zwave.gen.command_class Thermostat
+    mix zwave.gen.command_class ThermostatMode 40
 
   The pattern is
 
-    mix zwave.gen.command_class CommandClassModule
+    mix zwave.gen.command_class CommandClassModule CommandClassID
 
   Flags:
 
@@ -21,18 +21,40 @@ defmodule Mix.Tasks.Zwave.Gen.CommandClass do
   @switches [no_save: :boolean, target_dir: :string]
 
   def run(args) do
-    {opts, [command_class], _} = OptionParser.parse(args, switches: @switches)
+    {opts, args, _} = OptionParser.parse(args, switches: @switches)
+
+    command_class = args |> Enum.at(0) |> maybe_prompt_for_command_class_name()
+    cc_id = args |> Enum.at(1) |> maybe_prompt_for_command_class_id()
 
     command_class_name_str_snake_case = Macro.underscore(command_class)
 
     target_path = Path.join(get_target(opts), "#{command_class_name_str_snake_case}.ex")
 
-    copy_files(target_path, command_class, opts)
+    copy_files(target_path, command_class, cc_id, opts)
   end
 
-  defp copy_files(target_path, command_class, opts) do
+  defp maybe_prompt_for_command_class_name(name \\ nil) do
+    name = name || String.trim(Mix.shell().prompt("Command Class:"))
+
+    if name == "" do
+      maybe_prompt_for_command_class_name()
+    else
+      name
+    end
+  end
+
+  defp maybe_prompt_for_command_class_id(id \\ nil) do
+    id = id || String.trim(Mix.shell().prompt("Command Class ID (hex):"))
+
+    case Regex.run(~r/^0?x?([0-9A-Fa-f]{2})$/, id) do
+      [_, id] -> "0x" <> String.upcase(id)
+      _ -> maybe_prompt_for_command_class_id()
+    end
+  end
+
+  defp copy_files(target_path, command_class, cc_id, opts) do
     template_file =
-      Path.join(Application.app_dir(:grizzly), "/priv/templates/zwave.gen/command_class.ex")
+      Path.join(Application.app_dir(:grizzly), "/priv/templates/zwave.gen/command_class.eex")
 
     command_class_module = Module.concat(Grizzly.ZWave.CommandClasses, command_class)
     command_class_name = String.to_atom(Macro.underscore(command_class))
@@ -40,14 +62,22 @@ defmodule Mix.Tasks.Zwave.Gen.CommandClass do
     bindings = [
       command_class_name: command_class_name,
       command_class_module: command_class_module,
-      command_class: command_class
+      command_class: command_class,
+      command_class_id: cc_id
     ]
 
     to_out(target_path, template_file, bindings, opts)
   end
 
   defp to_out(target_path, template_file, bindings, opts) do
-    evaled = EEx.eval_file(template_file, bindings)
+    evaled = EEx.eval_file(template_file, bindings, trim: true)
+
+    evaled =
+      try do
+        Code.format_string!(evaled)
+      rescue
+        _ -> evaled
+      end
 
     if Keyword.has_key?(opts, :no_save) do
       IO.puts(evaled)
