2ca259af57516a6389329d6c6f9edb761aca7c31
Ben Youngblood <bx.youngblood@gmail.com>
2023-11-14 10:14:17 -0700

    Fix User Code Set when User ID Status is 0x00 (#868)
    The Z-Wave spec states that the User Code field must be set to
    0x00000000 when User ID Status is 0x00 (available).

--- lib/grizzly/zwave/commands/user_code_set.ex
+++ lib/grizzly/zwave/commands/user_code_set.ex
@@ -40,6 +40,15 @@ defmodule Grizzly.ZWave.Commands.UserCodeSet do
     user_id_status = Command.param!(command, :user_id_status)
     user_code = Command.param!(command, :user_code)
 
+    # CC:0063.01.01.11.009 - The User Code field MUST be set to 0x00000000 (4 bytes)
+    # when User ID Status is equal to 0x00 (Available).
+    user_code =
+      if user_id_status == :available do
+        <<0x00, 0x00, 0x00, 0x00>>
+      else
+        user_code
+      end
+
     <<user_id, UserCode.user_id_status_to_byte(user_id_status)>> <> user_code
   end
 
