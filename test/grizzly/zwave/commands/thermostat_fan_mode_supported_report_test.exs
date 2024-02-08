19a4831e3706299dea29d77cd8ea4c72acb9713a
Ben Youngblood <ben.youngblood@smartrent.com>
2024-02-08 11:08:56 -0700

    Implement Thermostat Fan Mode Supported Get/Report

---
+++ test/grizzly/zwave/commands/thermostat_fan_mode_supported_report_test.exs
@@ -0,0 +1,22 @@
+defmodule Grizzly.ZWave.Commands.ThermostatFanModeSupportedReportTest do
+  use ExUnit.Case, async: true
+
+  alias Grizzly.ZWave.Commands.ThermostatFanModeSupportedReport
+
+  test "encodes params correctly" do
+    modes = [:auto_low, :low, :auto_high, :auto_medium]
+
+    {:ok, cmd} = ThermostatFanModeSupportedReport.new(modes: modes)
+
+    expected_binary = <<0b00010111>>
+
+    assert expected_binary == ThermostatFanModeSupportedReport.encode_params(cmd)
+  end
+
+  test "decodes params correctly" do
+    binary = <<0b00010111>>
+
+    assert {:ok, [modes: [:auto_low, :low, :auto_high, :auto_medium]]} ==
+             ThermostatFanModeSupportedReport.decode_params(binary)
+  end
+end
