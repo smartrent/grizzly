01e7445bb3bc7bb5288216a3acb7d90c642ee563
Jean-Francois Cloutier <jean.f.cloutier@gmail.com>
2025-01-14 15:37:58 -0500

    Fix wrong command bytes for configuration info get and report (#1035)

---
+++ test/grizzly/zwave/commands/configuration_info_report_test.exs
@@ -0,0 +1,24 @@
+defmodule Grizzly.ZWave.Commands.ConfigurationInfoReportTest do
+  use ExUnit.Case, async: true
+
+  alias Grizzly.ZWave.Commands.ConfigurationInfoReport
+
+  test "creates the command and validates params" do
+    params = [param_number: 2, info: "something", reports_to_follow: 0]
+    {:ok, _command} = ConfigurationInfoReport.new(params)
+  end
+
+  test "encodes params correctly" do
+    params = [param_number: 2, info: "something", reports_to_follow: 0]
+    {:ok, command} = ConfigurationInfoReport.new(params)
+    expected_params_binary = <<0x02::16, 0x00>> <> "something"
+    assert expected_params_binary == ConfigurationInfoReport.encode_params(command)
+  end
+
+  test "decodes params correctly" do
+    params_binary = <<0x02::16, 0x00>> <> "something"
+    {:ok, params} = ConfigurationInfoReport.decode_params(params_binary)
+    assert Keyword.get(params, :param_number) == 2
+    assert Keyword.get(params, :info) == "something"
+  end
+end
