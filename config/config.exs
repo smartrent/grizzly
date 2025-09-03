0ca0e202db111d656d3bf4134194f1e4aa2f50b3
Ben Youngblood <bx.youngblood@gmail.com>
2025-09-03 14:11:52 -0700

    CI: Enable reruns of failed workflows (#1119)

--- config/config.exs
+++ config/config.exs
@@ -3,7 +3,9 @@ import Config
 config :logger, level: :debug
 
 if config_env() == :test do
-  config :junit_formatter, report_dir: File.cwd!()
+  config :junit_formatter,
+    report_dir: File.cwd!(),
+    include_filename?: true
 
   config :grizzly,
     unsolicited_server_health_check_interval: 1000,
