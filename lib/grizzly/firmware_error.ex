9df13a296818d852df482e2de27a9d967a810cb2
Ben Youngblood <ben.youngblood@smartrent.com>
2025-11-19 16:15:27 -0700

    Remove unused exception

--- lib/grizzly/firmware_error.ex
+++
@@ -1,8 +0,0 @@
-defmodule Grizzly.FirmwareError do
-  @moduledoc """
-  Z-Wave firmware exception
-  """
-  @type t() :: %__MODULE__{message: String.t(), stack_trace: String.t(), fatal?: boolean()}
-
-  defexception [:message, :stack_trace, :fatal?]
-end
