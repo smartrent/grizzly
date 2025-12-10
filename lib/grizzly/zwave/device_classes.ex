17cb0b99a9447c208f8a062345e380146a11a7be
Ben Youngblood <ben.youngblood@smartrent.com>
2025-12-10 15:53:32 -0700

    Use runtime map lookups instead of generating functions
    Using code generation in modules like `IconType` and `CommandClasses`
    considerably increases the size of the generated bytecode compared to
    doing map lookups at runtime.

--- lib/grizzly/zwave/device_classes.ex
+++ lib/grizzly/zwave/device_classes.ex
@@ -10,67 +10,44 @@ defmodule Grizzly.ZWave.DeviceClasses do
       specific_device_class_mappings: 0
     ]
 
-  basic_mappings = basic_device_class_mappings()
-  generic_mappings = generic_device_class_mappings()
-  specific_mappings = specific_device_class_mappings()
-
-  basic_classes_union =
-    basic_mappings
-    |> Enum.map(&elem(&1, 1))
-    |> Enum.reverse()
-    |> Enum.reduce(&{:|, [], [&1, &2]})
-
-  generic_classes_union =
-    generic_mappings
-    |> Enum.map(&elem(&1, 1))
-    |> Enum.reverse()
-    |> Enum.reduce(&{:|, [], [&1, &2]})
-
-  specific_classes_union =
-    specific_mappings
-    |> Enum.map(&elem(&1, 2))
-    |> Enum.reverse()
-    |> Enum.uniq()
-    |> Enum.reduce(&{:|, [], [&1, &2]})
-
-  @type basic_device_class :: unquote(basic_classes_union)
-  @type generic_device_class :: unquote(generic_classes_union)
-  @type specific_device_class :: unquote(specific_classes_union)
+  @type basic_device_class :: atom()
+  @type generic_device_class :: atom()
+  @type specific_device_class :: atom()
 
   @doc """
   Try to make a basic device class from a byte
   """
   @spec decode_basic(byte()) :: basic_device_class() | :unknown
-  for {byte, device_class} <- basic_mappings do
-    def decode_basic(unquote(byte)), do: unquote(device_class)
+  def decode_basic(byte) do
+    Enum.find_value(basic_device_class_mappings(), :unknown, fn {dc, b} ->
+      if b == byte, do: dc
+    end)
   end
-
-  def decode_basic(_byte), do: :unknown
 
   @doc """
   Make a byte from a device class
   """
   @spec encode_basic(basic_device_class()) :: byte()
-  for {byte, device_class} <- basic_mappings do
-    def encode_basic(unquote(device_class)), do: unquote(byte)
+  def encode_basic(device_class) do
+    Map.fetch!(basic_device_class_mappings(), device_class)
   end
 
   @doc """
   Try to get the generic device class for the byte
   """
   @spec decode_generic(byte()) :: generic_device_class() | :unknown
-  for {byte, device_class} <- generic_mappings do
-    def decode_generic(unquote(byte)), do: unquote(device_class)
+  def decode_generic(byte) do
+    Enum.find_value(generic_device_class_mappings(), :unknown, fn {dc, b} ->
+      if b == byte, do: dc
+    end)
   end
-
-  def decode_generic(_byte), do: :unknown
 
   @doc """
   Turn the generic device class into a byte
   """
   @spec encode_generic(generic_device_class()) :: byte()
-  for {byte, device_class} <- generic_mappings do
-    def encode_generic(unquote(device_class)), do: unquote(byte)
+  def encode_generic(device_class) do
+    Map.fetch!(generic_device_class_mappings(), device_class)
   end
 
   @doc """
@@ -78,19 +55,17 @@ defmodule Grizzly.ZWave.DeviceClasses do
   """
   @spec decode_specific(generic_device_class(), byte()) ::
           specific_device_class() | :unknown
-  for {gen_class, byte, spec_class} <- specific_mappings do
-    def decode_specific(unquote(gen_class), unquote(byte)),
-      do: unquote(spec_class)
+  def decode_specific(gen_class, byte) do
+    Map.get(specific_device_class_mappings(), {gen_class, byte}, :unknown)
   end
-
-  def decode_specific(_, _byte), do: :unknown
 
   @doc """
   Make the specific device class into a byte
   """
   @spec encode_specific(generic_device_class(), specific_device_class()) :: byte()
-  for {gen_class, byte, spec_class} <- specific_mappings do
-    def encode_specific(unquote(gen_class), unquote(spec_class)),
-      do: unquote(byte)
+  def encode_specific(gen_class, spec_class) do
+    Enum.find_value(specific_device_class_mappings(), fn {{g, b}, s} ->
+      if g == gen_class and s == spec_class, do: b
+    end)
   end
 end
