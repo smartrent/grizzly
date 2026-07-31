1576fa94d589b213e9af1c11e5e0923696d0dbc4
Noah Betzen <noah@nezteb.net>
2026-07-31 13:49:08 -0600

    Account for missing YMM notification type(s)

--- test/grizzly/zwave/notifications_test.exs
+++ test/grizzly/zwave/notifications_test.exs
@@ -1,5 +1,36 @@
 defmodule Grizzly.ZWave.NotificationsTest do
   use ExUnit.Case, async: true
 
+  alias Grizzly.ZWave.Notifications
+
   doctest Grizzly.ZWave.Notifications
+
+  describe "decode_event_params/3 for access_control credential usage events" do
+    test "credential_unlock_operation" do
+      assert Notifications.decode_event_params(
+               :access_control,
+               :credential_unlock_operation,
+               <<0, 33, 1, 1, 0, 4>>
+             ) ==
+               {:ok, [user_id: 33, credentials_used: [%{type: :pin_code, slot_id: 4}]]}
+    end
+
+    test "credential_lock_operation" do
+      assert Notifications.decode_event_params(
+               :access_control,
+               :credential_lock_operation,
+               <<0, 33, 1, 1, 0, 4>>
+             ) ==
+               {:ok, [user_id: 33, credentials_used: [%{type: :pin_code, slot_id: 4}]]}
+    end
+
+    test "non_access_credential_used" do
+      assert Notifications.decode_event_params(
+               :access_control,
+               :non_access_credential_used,
+               <<0, 33, 1, 1, 0, 4>>
+             ) ==
+               {:ok, [user_id: 33, credentials_used: [%{type: :pin_code, slot_id: 4}]]}
+    end
+  end
 end
